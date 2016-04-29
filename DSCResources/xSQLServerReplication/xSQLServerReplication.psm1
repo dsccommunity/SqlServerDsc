b8e0fd5a9973a3d94d39b59a65694d4bd2b900e8
Arturas Kuznecovas <arturas.k@hotmail.com>
2016-04-29 16:11:51 +0300

    xSQLServerReplication: * Moved detection code to from Test function to Get; * Updated Test function to compare imput values and state returned by Get; * Moved AppDomain variable to script scope;

--- DSCResources/xSQLServerReplication/xSQLServerReplication.psm1
+++ DSCResources/xSQLServerReplication/xSQLServerReplication.psm1
@@ -1,4 +1,6 @@
-﻿Function Get-TargetResource
+﻿$dom = [AppDomain]::CreateDomain('xSQLServerReplication')
+
+Function Get-TargetResource
 {
     [CmdletBinding()]
     [OutputType([System.Collections.Hashtable])]
@@ -6,10 +8,6 @@
         [parameter(Mandatory = $true)]
         [System.String]
         $InstanceName,
-
-        [ValidateSet('Present', 'Absent')]
-        [System.String]
-        $Ensure = 'Present',
 
         [parameter(Mandatory = $true)]
         [ValidateSet('Local', 'Remote')]
@@ -20,42 +18,57 @@
         [System.Management.Automation.PSCredential]
         $AdminLinkCredentials,
 
-        [System.String]
-        $DistributionDBName = 'distribution',
-
-        [System.String]
-        $RemoteDistributor,
-
         [parameter(Mandatory = $true)]
         [System.String]
-        $WorkingDirectory,
-
-        [System.Boolean]
-        $UseTrustedConnection = $true,
-
-        [System.Boolean]
-        $UninstallWithForce = $true
+        $WorkingDirectory
     )
 
-    if(Test-TargetResource $InstanceName $Ensure $DistributorMode $AdminLinkCredentials $DistributionDBName $RemoteDistributor $WorkingDirectory $UseTrustedConnection $UninstallWithForce)
+    $WorkingDirectory = ''
+    $Ensure = 'Absent'
+
+    $sqlMajorVersion = Get-SqlServerMajorVersion $InstanceName
+    $connInfo = $dom.Load("Microsoft.SqlServer.ConnectionInfo, Version=$sqlMajorVersion.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91")
+    Write-Verbose "Loaded assembly: $($connInfo.FullName)"
+    $rmo = $dom.Load("Microsoft.SqlServer.Rmo, Version=$sqlMajorVersion.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91")
+    Write-Verbose "Loaded assembly: $($rmo.FullName)"
+
+    if($InstanceName -eq "MSSQLSERVER")
     {
-        $Ensure = 'Present'
+        $localSqlName = $env:COMPUTERNAME
     }
     else
     {
-        $Ensure = 'Absent'
+        $localSqlName = "$($env:COMPUTERNAME)\$InstanceName"
     }
-    
+
+    $localConnection = New-Object $connInfo.GetType('Microsoft.SqlServer.Management.Common.ServerConnection') $localSqlName
+    $localReplicationServer = New-Object $rmo.GetType('Microsoft.SqlServer.Replication.ReplicationServer') $localConnection
+
+    if($localReplicationServer.IsDistributor -eq $true)
+    {
+        $Ensure = 'Present'
+        $DistributorMode = 'Local'
+    }
+    elseif($localReplicationServer.IsPublisher -eq $true)
+    {
+        $Ensure = 'Present'
+        $DistributorMode = 'Remote'
+    }
+
+    if($Ensure -eq 'Preset')
+    {
+        $DistributionDBName = $localReplicationServer.DistributionDatabase
+        $RemoteDistributor = $localReplicationServer.DistributionServer
+        $WorkingDirectory = $localReplicationServer.WorkingDirectory
+    }
+               
     $returnValue = @{
         InstanceName = $InstanceName
         Ensure = $Ensure
         DistributorMode = $DistributorMode
-        AdminLinkCredentials = $AdminLinkCredentials
         DistributionDBName = $DistributionDBName
         RemoteDistributor = $RemoteDistributor
         WorkingDirectory = $WorkingDirectory
-        UseTrustedConnection = $UseTrustedConnection
-        UninstallWithForce = $UninstallWithForce
     }
     
     return $returnValue
@@ -106,70 +119,64 @@ Function Set-TargetResource
 
     $sqlMajorVersion = Get-SqlServerMajorVersion $InstanceName
 
-    try
-    {
-        $dom = [AppDomain]::CreateDomain("xSQLServerReplication_$sqlMajorVersion")
-        $connInfo = $dom.Load("Microsoft.SqlServer.ConnectionInfo, Version=$sqlMajorVersion.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91")
-        $rmo = $dom.Load("Microsoft.SqlServer.Rmo, Version=$sqlMajorVersion.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91")
+    $connInfo = $dom.Load("Microsoft.SqlServer.ConnectionInfo, Version=$sqlMajorVersion.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91")
+    Write-Verbose "Loaded assembly: $($connInfo.FullName)"
+    $rmo = $dom.Load("Microsoft.SqlServer.Rmo, Version=$sqlMajorVersion.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91")
+    Write-Verbose "Loaded assembly: $($rmo.FullName)"
 
-        if($InstanceName -eq "MSSQLSERVER")
+    if($InstanceName -eq "MSSQLSERVER")
+    {
+        $localSqlName = $env:COMPUTERNAME
+    }
+    else
+    {
+        $localSqlName = "$($env:COMPUTERNAME)\$InstanceName"
+    }
+
+    $localConnection = New-Object $connInfo.GetType('Microsoft.SqlServer.Management.Common.ServerConnection') $localSqlName
+    $localReplicationServer = New-Object $rmo.GetType('Microsoft.SqlServer.Replication.ReplicationServer') $localConnection
+
+    if($Ensure -eq 'Present')
+    {
+        if($DistributorMode -eq 'Local' -and $localReplicationServer.IsDistributor -eq $false)
         {
-            $localSqlName = $env:COMPUTERNAME
+            Write-Verbose "Local distribution will be configured ..."
+            $distributionDB = New-Object $rmo.GetType('Microsoft.SqlServer.Replication.DistributionDatabase') $DistributionDBName, $localConnection
+            $localReplicationServer.InstallDistributor($AdminLinkCredentials.Password, $distributionDB)
+
+            $distributorPublisher = New-object $rmo.GetType('Microsoft.SqlServer.Replication.DistributionPublisher') $localSqlName, $localConnection
+            $distributorPublisher.DistributionDatabase = $DistributionDBName
+            $distributorPublisher.WorkingDirectory = $WorkingDirectory
+            $distributorPublisher.PublisherSecurity.WindowsAuthentication = $UseTrustedConnection
+            $distributorPublisher.Create()
+        }
+            
+        if($DistributorMode -eq 'Remote' -and $localReplicationServer.IsPublisher -eq $false)
+        {
+            Write-Verbose "Remote distribution will be configured ..."
+
+            $remoteConnection = New-Object $connInfo.GetType('Microsoft.SqlServer.Management.Common.ServerConnection') $RemoteDistributor
+
+            $distributorPublisher = New-object $rmo.GetType('Microsoft.SqlServer.Replication.DistributionPublisher') $localSqlName, $remoteConnection
+            $distributorPublisher.DistributionDatabase = $DistributionDBName
+            $distributorPublisher.WorkingDirectory = $WorkingDirectory
+            $distributorPublisher.PublisherSecurity.WindowsAuthentication = $UseTrustedConnection
+            $distributorPublisher.Create()
+
+            $localReplicationServer.InstallDistributor($RemoteDistributor, $AdminLinkCredentials.Password)
+        }
+    }
+    else #'Absent'
+    {
+        if($localReplicationServer.IsDistributor -eq $true -or $localReplicationServer.IsPublisher -eq $true)
+        {
+            Write-Verbose "Distribution will be removed ..."
+            $localReplicationServer.UninstallDistributor($UninstallWithForce)
         }
         else
         {
-            $localSqlName = "$($env:COMPUTERNAME)\$InstanceName"
+            Write-Verbose "Distribution is not configured on this instance."
         }
-
-        $localConnection = New-Object $connInfo.GetType('Microsoft.SqlServer.Management.Common.ServerConnection') $localSqlName
-        $localReplicationServer = New-Object $rmo.GetType('Microsoft.SqlServer.Replication.ReplicationServer') $localConnection
-
-        if($Ensure -eq 'Present')
-        {
-            if($DistributorMode -eq 'Local' -and $localReplicationServer.IsDistributor -eq $false)
-            {
-                Write-Verbose "Local distribution will be configured ..."
-                $distributionDB = New-Object $rmo.GetType('Microsoft.SqlServer.Replication.DistributionDatabase') $DistributionDBName, $localConnection
-                $localReplicationServer.InstallDistributor($AdminLinkCredentials.Password, $distributionDB)
-
-                $distributorPublisher = New-object $rmo.GetType('Microsoft.SqlServer.Replication.DistributionPublisher') $localSqlName, $localConnection
-                $distributorPublisher.DistributionDatabase = $DistributionDBName
-                $distributorPublisher.WorkingDirectory = $WorkingDirectory
-                $distributorPublisher.PublisherSecurity.WindowsAuthentication = $UseTrustedConnection
-                $distributorPublisher.Create()
-            }
-            
-            if($DistributorMode -eq 'Remote' -and $localReplicationServer.IsPublisher -eq $false)
-            {
-                Write-Verbose "Remote distribution will be configured ..."
-
-                $remoteConnection = New-Object $connInfo.GetType('Microsoft.SqlServer.Management.Common.ServerConnection') $RemoteDistributor
-
-                $distributorPublisher = New-object $rmo.GetType('Microsoft.SqlServer.Replication.DistributionPublisher') $localSqlName, $remoteConnection
-                $distributorPublisher.DistributionDatabase = $DistributionDBName
-                $distributorPublisher.WorkingDirectory = $WorkingDirectory
-                $distributorPublisher.PublisherSecurity.WindowsAuthentication = $UseTrustedConnection
-                $distributorPublisher.Create()
-
-                $localReplicationServer.InstallDistributor($RemoteDistributor, $AdminLinkCredentials.Password)
-            }
-        }
-        else #'Absent'
-        {
-            if($localReplicationServer.IsDistributor -eq $true -or $localReplicationServer.IsPublisher -eq $true)
-            {
-                Write-Verbose "Distribution will be removed ..."
-                $localReplicationServer.UninstallDistributor($UninstallWithForce)
-            }
-            else
-            {
-                Write-Verbose "Distribution is not configured on this instance."
-            }
-        }
-    }
-    finally
-    {
-        [AppDomain]::Unload($dom)
     }
 }
 
@@ -212,53 +219,18 @@ Function Test-TargetResource
         $UninstallWithForce = $true
     )
 
-    $sqlMajorVersion = Get-SqlServerMajorVersion $InstanceName
     $result = $false
+    $state = Get-TargetResource -InstanceName $InstanceName -DistributorMode $DistributorMode -AdminLinkCredentials $AdminLinkCredentials -WorkingDirectory $WorkingDirectory
 
-    try
+    if($Ensure -eq 'Absent' -and $state.Ensure -eq 'Absent')
     {
-        $dom = [AppDomain]::CreateDomain("xSQLServerReplication_$sqlMajorVersion")
-        $connInfo = $dom.Load("Microsoft.SqlServer.ConnectionInfo, Version=$sqlMajorVersion.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91")
-        $rmo = $dom.Load("Microsoft.SqlServer.Rmo, Version=$sqlMajorVersion.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91")
-
-        if($InstanceName -eq "MSSQLSERVER")
-        {
-            $localSqlName = $env:COMPUTERNAME
-        }
-        else
-        {
-            $localSqlName = "$($env:COMPUTERNAME)\$InstanceName"
-        }
-
-        $localConnection = New-Object $connInfo.GetType('Microsoft.SqlServer.Management.Common.ServerConnection') $localSqlName
-        $localReplicationServer = New-Object $rmo.GetType('Microsoft.SqlServer.Replication.ReplicationServer') $localConnection
-
-        if($Ensure -eq 'Present')
-        {
-            if($DistributorMode -eq 'Local' -and $localReplicationServer.IsDistributor -eq $true)
-            {
-                $result = $true
-            }
-
-            if($DistributorMode -eq 'Remote' -and $localReplicationServer.IsPublisher -eq $true)
-            {
-                $result = $true
-            }
-
-        }
-        else #Absent
-        {
-            if($localReplicationServer.IsDistributor -eq $false -and $localReplicationServer.IsPublisher -eq $false)
-            {
-                $result = $true
-            }
-        }
+        $result = $true
     }
-    finally
+    elseif($Ensure -eq 'Present' -and $state.Ensure -eq 'Present' -and $state.DistributorMode -eq $DistributorMode)
     {
-        [AppDomain]::Unload($dom)
+        $result = $true
     }
-    
+          
     return $result
 }
 
