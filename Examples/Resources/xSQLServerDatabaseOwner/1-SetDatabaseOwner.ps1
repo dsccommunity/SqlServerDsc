3c5eda89a67aa1a5d6a897b28b6c234aeac7ef11
luigilink <jcdrouhin@yahoo.fr>
2016-11-21 11:24:46 +0100

    Remove Extra indentation in Example files

--- Examples/Resources/xSQLServerDatabaseOwner/1-SetDatabaseOwner.ps1
+++ Examples/Resources/xSQLServerDatabaseOwner/1-SetDatabaseOwner.ps1
@@ -4,39 +4,39 @@
     is "Owner" of SQL database "AdventureWorks". 
 #>
 
-    Configuration Example 
+Configuration Example 
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $SysAdminAccount
+    )
+
+    Import-DscResource -ModuleName xSqlServer
+
+    node localhost 
     {
-        param
-        (
-            [Parameter(Mandatory = $true)]
-            [System.Management.Automation.PSCredential]
-            [System.Management.Automation.Credential()]
-            $SysAdminAccount
-        )
-
-        Import-DscResource -ModuleName xSqlServer
-
-        node localhost 
+        xSQLServerLogin Add_SqlServerLogin_SQLAdmin
         {
-            xSQLServerLogin Add_SqlServerLogin_SQLAdmin
-            {
-                DependsOn = '[xSqlServerSetup]SETUP_SqlMSSQLSERVER'
-                Ensure = 'Present'
-                Name = 'CONTOSO\SQLAdmin'
-                LoginType = 'WindowsUser'        
-                SQLServer = 'SQLServer'
-                SQLInstanceName = 'DSC'
-                PsDscRunAsCredential = $SysAdminAccount
-            }
+            DependsOn = '[xSqlServerSetup]SETUP_SqlMSSQLSERVER'
+            Ensure = 'Present'
+            Name = 'CONTOSO\SQLAdmin'   
+            LoginType = 'WindowsUser'        
+            SQLServer = 'SQLServer'
+            SQLInstanceName = 'DSC'
+            PsDscRunAsCredential = $SysAdminAccount
+        }
 
-            xSQLServerDatabaseOwner Set_SqlDatabaseOwner_SQLAdmin
-            {
-                DependsOn = '[xSQLServerLogin]Add_SqlServerLogin_SQLAdmin'
-                Name = 'CONTOSO\SQLAdmin'
-                Database = 'AdventureWorks'
-                SQLServer = 'SQLServer'
-                SQLInstanceName = 'DSC'
-                PsDscRunAsCredential = $SysAdminAccount
-            }
+        xSQLServerDatabaseOwner Set_SqlDatabaseOwner_SQLAdmin
+        {
+            DependsOn = '[xSQLServerLogin]Add_SqlServerLogin_SQLAdmin'
+            Name = 'CONTOSO\SQLAdmin'
+            Database = 'AdventureWorks'
+            SQLServer = 'SQLServer'
+            SQLInstanceName = 'DSC'
+            PsDscRunAsCredential = $SysAdminAccount
         }
     }
+}
