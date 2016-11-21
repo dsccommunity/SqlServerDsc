f80224a0bc4a1e3587f7be8d659a1bb4bfabd128
luigilink <jcdrouhin@yahoo.fr>
2016-11-21 11:24:46 +0100

    Remove Extra indentation in Example files

--- Examples/Resources/xSQLServerDatabasePermissions/1-AddDatabasePermissions.ps1
+++ Examples/Resources/xSQLServerDatabasePermissions/1-AddDatabasePermissions.ps1
@@ -4,39 +4,39 @@
     has "Connect" and "Update" SQL Permissions for database "AdventureWorks". 
 #>
 
-    Configuration Example 
-    {
-        param(
-            [Parameter(Mandatory = $true)]
-            [PSCredential]
-            $SysAdminAccount
-        )
-        
-        Import-DscResource -ModuleName xSqlServer
+Configuration Example 
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [PSCredential]
+        $SysAdminAccount
+    )
+    
+    Import-DscResource -ModuleName xSqlServer
 
-        node localhost {
-            xSQLServerLogin Add_SqlServerLogin_SQLAdmin
-            {
-                DependsOn = '[xSqlServerSetup]SETUP_SqlMSSQLSERVER'
-                Ensure = 'Present'
-                Name = 'CONTOSO\SQLAdmin'
-                LoginType = 'WindowsUser'        
-                SQLServer = 'SQLServer'
-                SQLInstanceName = 'DSC'
-                PsDscRunAsCredential = $SysAdminAccount
-            }
+    node localhost {
+        xSQLServerLogin Add_SqlServerLogin_SQLAdmin
+        {
+            DependsOn = '[xSqlServerSetup]SETUP_SqlMSSQLSERVER'
+            Ensure = 'Present'
+            Name = 'CONTOSO\SQLAdmin'
+            LoginType = 'WindowsUser'        
+            SQLServer = 'SQLServer'
+            SQLInstanceName = 'DSC'
+            PsDscRunAsCredential = $SysAdminAccount
+        }
 
-            xSQLServerDatabasePermissions Add_SqlDatabasePermissions_SQLAdmin
-            {
-                DependsOn = '[xSQLServerLogin]Add_SqlServerLogin_SQLAdmin'
-                Ensure = 'Present'
-                Name = 'CONTOSO\SQLAdmin'
-                Database = 'AdventureWorks'
-                PermissionState = 'Grant'
-                Permissions = "Connect","Update"
-                SQLServer = 'SQLServer'
-                SQLInstanceName = 'DSC'
-                PsDscRunAsCredential = $SysAdminAccount
-            }
+        xSQLServerDatabasePermissions Add_SqlDatabasePermissions_SQLAdmin
+        {
+            DependsOn = '[xSQLServerLogin]Add_SqlServerLogin_SQLAdmin'
+            Ensure = 'Present'
+            Name = 'CONTOSO\SQLAdmin'
+            Database = 'AdventureWorks'
+            PermissionState = 'Grant'
+            Permissions = "Connect","Update"
+            SQLServer = 'SQLServer'
+            SQLInstanceName = 'DSC'
+            PsDscRunAsCredential = $SysAdminAccount
         }
     }
+}
--- Examples/Resources/xSQLServerDatabasePermissions/2-RemoveDatabasePermissions.ps1
+++ Examples/Resources/xSQLServerDatabasePermissions/2-RemoveDatabasePermissions.ps1
@@ -4,39 +4,39 @@
     hasn't "Select" and "Create Table" SQL Permissions for database "AdventureWorks". 
 #>
 
-    Configuration Example 
-    {
-        param(
-            [Parameter(Mandatory = $true)]
-            [PSCredential]
-            $SysAdminAccount
-        )
-        
-        Import-DscResource -ModuleName xSqlServer
+Configuration Example 
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [PSCredential]
+        $SysAdminAccount
+    )
+    
+    Import-DscResource -ModuleName xSqlServer
 
-        node localhost {
-            xSQLServerLogin Add_SqlServerLogin_SQLAdmin
-            {
-                DependsOn = '[xSqlServerSetup]SETUP_SqlMSSQLSERVER'
-                Ensure = 'Present'
-                Name = 'CONTOSO\SQLAdmin'
-                LoginType = 'WindowsUser'        
-                SQLServer = 'SQLServer'
-                SQLInstanceName = 'DSC'
-                PsDscRunAsCredential = $SysAdminAccount
-            }
+    node localhost {
+        xSQLServerLogin Add_SqlServerLogin_SQLAdmin
+        {
+            DependsOn = '[xSqlServerSetup]SETUP_SqlMSSQLSERVER'
+            Ensure = 'Present'
+            Name = 'CONTOSO\SQLAdmin'
+            LoginType = 'WindowsUser'        
+            SQLServer = 'SQLServer'
+            SQLInstanceName = 'DSC'
+            PsDscRunAsCredential = $SysAdminAccount
+        }
 
-            xSQLServerDatabasePermissions Add_SqlDatabasePermissions_SQLAdmin
-            {
-                DependsOn = '[xSQLServerLogin]Add_SqlServerLogin_SQLAdmin'
-                Ensure = 'Absent'
-                Name = 'CONTOSO\SQLAdmin'
-                Database = 'AdventureWorks'
-                PermissionState = 'Deny'
-                Permissions = "Select","Create Table"
-                SQLServer = 'SQLServer'
-                SQLInstanceName = 'DSC'
-                PsDscRunAsCredential = $SysAdminAccount
-            }
+        xSQLServerDatabasePermissions Add_SqlDatabasePermissions_SQLAdmin
+        {
+            DependsOn = '[xSQLServerLogin]Add_SqlServerLogin_SQLAdmin'
+            Ensure = 'Absent'
+            Name = 'CONTOSO\SQLAdmin'
+            Database = 'AdventureWorks'
+            PermissionState = 'Deny'
+            Permissions = "Select","Create Table"
+            SQLServer = 'SQLServer'
+            SQLInstanceName = 'DSC'
+            PsDscRunAsCredential = $SysAdminAccount
         }
     }
+}
