164e1e83b2bc6b92a0df6efea8b71736b82958ea
Daniel Reist <djreist@outlook.com>
2016-12-01 08:26:29 -0500

    Added test for Disable for a named instance

--- Tests/Unit/MSFT_xSQLServerAlwaysOnService.Tests.ps1
+++ Tests/Unit/MSFT_xSQLServerAlwaysOnService.Tests.ps1
@@ -162,6 +162,7 @@ try
                 
                 Set-TargetResource @enableHadrNamedInstance
                 Assert-MockCalled -ModuleName $script:DSCResourceName -CommandName Connect-SQL -Scope It -Times 1
+                Assert-MockCalled -ModuleName $script:DSCResourceName -CommandName Disable-SqlAlwaysOn -Scope It -Times 0
                 Assert-MockCalled -ModuleName $script:DSCResourceName -CommandName Enable-SqlAlwaysOn -Scope It -Times 1
                 Assert-MockCalled -ModuleName $script:DSCResourceName -CommandName Restart-SqlService -Scope It -Times 1
             }
