86b9d25325f37875ae87ac2543c8065725814d18
Johan Ljunggren <johan.ljunggren@shamn.se>
2017-01-07 10:04:02 +0100

    Changes to xSQLServer - Added common test xSQLServerCommon.Tests for xSQLServer module   - Now all markdown files will be style checked when tests are running in AppVeyor after sending in a pull request.   - Now all [Examples](/Examples/Resources) will be tested by compiling to a .mof file after sending in a pull request. - Changes to xSQLServerDatabaseOwner   - The example 'SetDatabaseOwner' can now compile, it wrongly had a `DependsOn` in the example. - Changes to SQLServerRole   - The examples 'AddServerRole' and 'RemoveServerRole' can now compile, it wrongly had a `DependsOn` in the example. - Changes to xSQLServerScript   - The examples 'AddServerRole' and 'RemoveServerRole' can now compile correctly. - Changes to CONTRIBUTING.md   - Added section 'Tests for examples files'   - Added section 'Tests for style check of Markdown files'   - Added section 'Documentation with Markdown'   - Added texts to section 'Tests' Added default rules to .markdownlint.json

--- Examples/Resources/xSQLServerRole/2-RemoveServerRole.ps1
+++ Examples/Resources/xSQLServerRole/2-RemoveServerRole.ps1
@@ -1,29 +1,28 @@
 <#
 .EXAMPLE
     This example shows how to ensure that the user account CONTOSO\SQLUser
-    does not have "setupadmin" SQL server role. 
+    does not have "setupadmin" SQL server role.
 #>
 
-    Configuration Example 
-    {
-        param(
-            [Parameter(Mandatory = $true)]
-            [PSCredential]
-            $SysAdminAccount
-        )
-        
-        Import-DscResource -ModuleName xSqlServer
+Configuration Example
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [PSCredential]
+        $SysAdminAccount
+    )
 
-        node localhost {
-            xSQLServerRole Add_SqlServerRole_SQLAdmin
-            {
-                DependsOn = '[xSQLServerLogin]Add_SqlServerLogin_SQLAdmin'
-                Ensure = 'Absent'
-                Name = 'CONTOSO\SQLUser'
-                ServerRole = "setupadmin"
-                SQLServer = 'SQLServer'
-                SQLInstanceName = 'DSC'
-                PsDscRunAsCredential = $SysAdminAccount
-            }
+    Import-DscResource -ModuleName xSqlServer
+
+    node localhost {
+        xSQLServerRole Remove_ServerRoleFromLogin
+        {
+            Ensure = 'Absent'
+            Name = 'CONTOSO\SQLUser'
+            ServerRole = "setupadmin"
+            SQLServer = 'SQLServer'
+            SQLInstanceName = 'DSC'
+            PsDscRunAsCredential = $SysAdminAccount
         }
     }
+}
