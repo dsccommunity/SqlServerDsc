2970cca88f843f5992f73b6bedc348a7154cf431
luigilink <jcdrouhin@yahoo.fr>
2017-02-22 14:20:27 +0100

    Add Examples files

---
+++ Examples/Resources/xSQLServerDatabaseRole/1-AddDatabaseRole.ps1
@@ -0,0 +1,31 @@
+<#
+.EXAMPLE
+    This example shows how to ensure that the user account CONTOSO\SQLAdmin
+    has "MyRole" and "MySecondRole" SQL database roles.
+#>
+Configuration Example
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $SysAdminAccount
+    )
+
+    Import-DscResource -ModuleName xSqlServer
+
+    node localhost
+    {
+        xSQLServerDatabaseRole Add_Database_Role
+        {
+            Ensure = 'Present'
+            SQLServer = 'SQLServer'
+            SQLInstanceName = 'DSC'
+            Name = 'CONTOSO\SQLAdmin'
+            Role = 'MyRole','MySecondRole'
+            Database = 'AdventureWorks'
+            PsDscRunAsCredential = $SysAdminAccount
+        }
+    }
+}
