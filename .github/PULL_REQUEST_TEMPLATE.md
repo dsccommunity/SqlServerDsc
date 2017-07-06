79032a18641a927d8f3879d668b44fba3eff5c4c
daniel-merchant <danielmerchant@gmail.com>
2017-07-06 17:15:59 +0100

    xSQLServerScript QueryTimeout
    Adds an optional QueryTimeout parameter to XSQLServerScript

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -12,10 +12,10 @@ with 'BREAKING CHANGE:',
 i.e. 'BREAKING CHANGE: xSQLServerSetup: My short description'
 -->
 **Pull Request (PR) description**
-<!-- Replace this with a description of your pull request -->
+xSQLServerScript Adding optional query timeout parameter
 
 **This Pull Request (PR) fixes the following issues:**
-<!-- Replace this with the list of issues or n/a. Use format: Fixes #123 -->
+Fixes #597
 
 **Task list:**
 <!--
@@ -23,8 +23,8 @@ To aid community reviewers in reviewing and merging your pull request (PR), plea
 the time to run through the below checklist.
 Change to [x] for each task in the task list that applies to your pull request (PR).
 -->
-- [ ] Change details added to Unreleased section of CHANGELOG.md?
-- [ ] Added/updated documentation, comment-based help and descriptions in .schema.mof files where appropriate?
-- [ ] Examples appropriately updated?
-- [ ] New/changed code adheres to [Style Guidelines](https://github.com/PowerShell/DscResources/blob/master/StyleGuidelines.md)?
-- [ ] [Unit and (optional) Integration tests](https://github.com/PowerShell/DscResources/blob/master/TestsGuidelines.md) created/updated where possible?
+- [x] Change details added to Unreleased section of CHANGELOG.md?
+- [x] Added/updated documentation, comment-based help and descriptions in .schema.mof files where appropriate?
+- [x] Examples appropriately updated?
+- [x] New/changed code adheres to [Style Guidelines](https://github.com/PowerShell/DscResources/blob/master/StyleGuidelines.md)?
+- [x] [Unit and (optional) Integration tests](https://github.com/PowerShell/DscResources/blob/master/TestsGuidelines.md) created/updated where possible?
--- DSCResources/MSFT_xSQLServerScript/MSFT_xSQLServerScript.psm1
+++ DSCResources/MSFT_xSQLServerScript/MSFT_xSQLServerScript.psm1
@@ -31,6 +31,11 @@ Import-Module -Name (Join-Path -Path (Split-Path -Path (Split-Path -Path $script
         Use a Windows PowerShell array to specify multiple variables and their values. For more information how to use this,
         please go to the help documentation for [Invoke-Sqlcmd](https://technet.microsoft.com/en-us/library/mt683370.aspx).
 
+	 .PARAMETER QueryTimeout
+        Specifies, as a integer the number of seconds after which the T-SQL script exeuction will time out.
+        This should default to 0 (no time out), but there are bugs in invoke-sqlcmd in some versions of SQL server where this is not respected
+        and the default is incorrecly set to 30 seconds.
+
     .OUTPUTS
         Hash table containing key 'GetResult' which holds the value of the result from the SQL script that was ran from the parameter 'GetFilePath'.
 #>
@@ -60,12 +65,16 @@ function Get-TargetResource
         [System.Management.Automation.Credential()]
         $Credential,
 
+		[Parameter(Mandatory = $false)]
+        [System.Int32]
+        $QueryTimeout,
+		
         [System.String[]]
         $Variable
     )
 
     $result = Invoke-SqlScript -ServerInstance $ServerInstance -SqlScriptPath $GetFilePath `
-                -Credential $Credential -Variable $Variable -ErrorAction Stop
+                -Credential $Credential -Variable $Variable -QueryTimeout $QueryTimeout -ErrorAction Stop
 
     $getResult = Out-String -InputObject $result
 
@@ -75,6 +84,7 @@ function Get-TargetResource
         GetFilePath = [System.String] $GetFilePath
         TestFilePath = [System.String] $TestFilePath
         Credential = [System.Object] $Credential
+        QueryTimeout = [System.Int32] $QueryTimeout
         Variable = [System.String[]] $Variable
         GetResult = [System.String[]] $getresult
     }
@@ -137,12 +147,16 @@ function Set-TargetResource
         [System.Management.Automation.Credential()]
         $Credential,
 
+		[Parameter(Mandatory = $false)]
+        [System.Int32]
+        $QueryTimeout,
+		
         [System.String[]]
         $Variable
     )
 
     Invoke-SqlScript -ServerInstance $ServerInstance -SqlScriptPath $SetFilePath `
-                -Credential $Credential -Variable $Variable -ErrorAction Stop
+                -Credential $Credential -Variable $Variable -QueryTimeout $QueryTimeout -ErrorAction Stop
 }
 
 <#
@@ -202,6 +216,10 @@ function Test-TargetResource
         [System.Management.Automation.Credential()]
         $Credential,
 
+		[Parameter(Mandatory = $false)]
+        [System.Int32]
+        $QueryTimeout,
+		
         [System.String[]]
         $Variable
     )
@@ -209,7 +227,7 @@ function Test-TargetResource
     try
     {
         $result = Invoke-SqlScript -ServerInstance $ServerInstance -SqlScriptPath $TestFilePath `
-                -Credential $Credential -Variable $Variable -ErrorAction Stop
+                -Credential $Credential -Variable $Variable -QueryTimeout $QueryTimeout -ErrorAction Stop
 
         if($null -eq $result)
         {
@@ -262,6 +280,10 @@ function Invoke-SqlScript
         [System.Management.Automation.Credential()]
         $Credential,
 
+		[Parameter(Mandatory = $false)]
+        [System.Int32]
+        $QueryTimeout, 
+		
         [System.String[]]
         $Variable
     )
