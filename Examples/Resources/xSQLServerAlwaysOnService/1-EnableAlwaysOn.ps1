bde34f35972786e01f323ba3ffbfd80a49373621
Johan Ljunggren <johlju@users.noreply.github.com>
2017-07-17 17:56:12 +0200

    xSQLServerAlwaysOnService: Added missing read property, fixed types, and added examples (#689)
    - Changes to xSQLServerAlwaysOnService
      - Added resource description in README.md.
      - Updated parameters descriptions in comment-based help, schema.mof and README.md.
      - Changed the datatype of the parameter to Uint32 so the same datatype is used in both the Get-/Test-/Set-TargetResource functions as in the schema.mof (issue #688).
      - Added read-only property IsHadrEnabled to schema.mof and the README.md (issue #687).
      - Minor cleanup of code.
      - Added examples (issue #633)
        - 1-EnableAlwaysOn.ps1
        - 2-DisableAlwaysOn.ps1

---
+++ Examples/Resources/xSQLServerAlwaysOnService/1-EnableAlwaysOn.ps1
@@ -0,0 +1,30 @@
+<#
+.EXAMPLE
+    This example shows how to enable SQL Server Always On high availability and
+    disaster recovery (HADR).
+#>
+Configuration Example
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $SysAdminAccount
+    )
+
+    Import-DscResource -ModuleName xSqlServer
+
+    node localhost
+    {
+        xSQLServerAlwaysOnService 'EnableAlwaysOn'
+        {
+            Ensure = 'Present'
+            SQLServer = 'SP23-VM-SQL1'
+            SQLInstanceName = 'MSSQLSERVER'
+            RestartTimeout = 120
+
+            PsDscRunAsCredential = $SysAdminAccount
+        }
+    }
+}
