05123ac4b2f4b0b4098e8a1f4d7a4883d0824565
Chris Gardner <chris@blackmarble.co.uk>
2017-09-26 21:26:59 +0100

    Update example to include collation setting.

--- Examples/Resources/xSQLServerDatabase/1-CreateDatabase.ps1
+++ Examples/Resources/xSQLServerDatabase/1-CreateDatabase.ps1
@@ -2,6 +2,9 @@
 .EXAMPLE
     This example shows how to create a database with
     the database name equal to 'Contoso'.
+
+    The second example shows how to create a database
+    with a different collation.
 #>
 Configuration Example
 {
@@ -23,6 +26,15 @@ Configuration Example
             SQLServer       = 'SQLServer'
             SQLInstanceName = 'DSC'
             Name            = 'Contoso'
+        }
+
+        xSQLServerDatabase Create_Database_with_different_collation
+        {
+            Ensure          = 'Present'
+            SQLServer       = 'SQLServer'
+            SQLInstanceName = 'DSC'
+            Name            = 'AdventureWorks'
+            Collation       = 'SQL_Latin1_General_Pref_CP850_CI_AS'
         }
     }
 }
