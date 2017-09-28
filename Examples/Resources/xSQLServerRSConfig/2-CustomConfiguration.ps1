da39d6aaff5c5b08a2350e1374698ff0800da08c
Marko Bozikovic <marko.bozikovic@gmail.com>
2017-09-28 23:18:19 +0100

    [#844] xSQLServerRSConfig examples

---
+++ Examples/Resources/xSQLServerRSConfig/2-CustomConfiguration.ps1
@@ -0,0 +1,31 @@
+<#
+.EXAMPLE
+    This example performs a custom	 SSRS configuration. It will initialize SSRS
+    and register custom Report Server Web Service and Report Manager URLs:
+
+    http://localhost:80/MyReportServer
+    https://localhost:443/MyReportServer (Report Server Web Service)
+
+    http://localhost:80/MyReports
+    https://localhost:443/MyReports (Report Manager)
+
+    Please note: this resource does not currently handle SSL bindings for HTTPS
+    endpoints.
+#>
+Configuration Example
+{
+    Import-DscResource -ModuleName xSqlServer
+
+    node localhost {
+        xSQLServerRSConfig DefaultConfiguration
+        {
+            InstanceName = 'MSSQLSERVER'
+            RSSQLServer = 'localhost'
+            RSSQLInstanceName = 'MSSQLSERVER'
+            ReportServerVirtualDir = 'MyReportServer'
+            ReportsVirtualDir = 'MyReports'
+            ReportServerReservedUrl = @('http://+:80', 'https://+:443')
+            ReportsReservedUrl = @('http://+:80', 'https://+:443')
+        }
+    }
+}
