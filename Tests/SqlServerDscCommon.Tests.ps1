2a2f7699cf80bbadcb7ae67d62cfaf6b8ade65cb
Johan Ljunggren <johlju@users.noreply.github.com>
2017-12-06 16:13:32 +0100

    Changes to SqlServerDsc
    - Added new common test (regression test) for validating the long path
      issue for compiling resources in Azure Automation

--- Tests/SqlServerDscCommon.Tests.ps1
+++ Tests/SqlServerDscCommon.Tests.ps1
@@ -72,4 +72,29 @@ Describe 'SqlServerDsc module common tests' {
             Import-Module -Name "$script:moduleRoot\SqlServerDsc.psd1" -Global -Force
         }
     }
+    
+    Context -Name 'When the resource should be used to compile a configuration in Azure Automation' {
+        $fullPathHardLimit = 129 # 129 characters is the current maxmium for a relative path to be able to compile configurations in Azure Automation.
+        $allModuleFiles = Get-ChildItem -Path $script:moduleRoot -Recurse
+
+        $testCaseModuleFile = @()
+        $allModuleFiles | ForEach-Object -Process {
+            $testCaseModuleFile += @(
+                @{
+                    FullRelativePath = $_.FullName -replace ($script:moduleRoot -replace '\\','\\')
+                }
+            )
+        }
+
+        It 'The length of the full path ''<FullRelativePath>'' should not exceed the max hard limit' -TestCases $testCaseModuleFile {
+            param
+            (
+                [Parameter()]
+                [System.String]
+                $FullRelativePath
+            )
+
+            $FullRelativePath.Length | Should -Not -BeGreaterThan $fullPathHardLimit
+        }
+    }
 }
