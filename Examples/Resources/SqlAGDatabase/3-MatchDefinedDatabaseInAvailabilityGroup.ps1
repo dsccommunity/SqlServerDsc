f26de8ad77e3548d72a5e5400bb602fd8596b62b
Shawn Melton <wshawnmelton@gmail.com>
2018-08-28 19:40:46 -0500

    update example for ag database

--- Examples/Resources/SqlAGDatabase/3-MatchDefinedDatabaseInAvailabilityGroup.ps1
+++ Examples/Resources/SqlAGDatabase/3-MatchDefinedDatabaseInAvailabilityGroup.ps1
@@ -68,6 +68,14 @@ Configuration Example
             PsDscRunAsCredential = $SqlAdministratorCredential
         }
 
+        SqlAlwaysOnService EnableHADR
+        {
+            Ensure               = 'Present'
+            InstanceName         = $Node.InstanceName
+            ServerName           = $Node.NodeName
+            PsDscRunAsCredential = $SqlAdministratorCredential
+        }
+
         if ( $Node.Role -eq 'PrimaryReplica' )
         {
             # Create the availability group on the instance tagged as the primary replica
@@ -77,7 +85,7 @@ Configuration Example
                 Name                 = $Node.AvailabilityGroupName
                 InstanceName         = $Node.SQLInstanceName
                 ServerName           = $Node.NodeName
-                DependsOn            = '[SqlServerEndpoint]HADREndpoint', '[SqlServerPermission]AddNTServiceClusSvcPermissions'
+                DependsOn            = '[SqlAlwaysOnService]EnableHADR', '[SqlServerEndpoint]HADREndpoint', '[SqlServerPermission]AddNTServiceClusSvcPermissions'
                 PsDscRunAsCredential = $SqlAdministratorCredential
             }
         }
@@ -94,6 +102,7 @@ Configuration Example
                 InstanceName               = $Node.SQLInstanceName
                 PrimaryReplicaServerName   = ( $AllNodes | Where-Object { $_.Role -eq 'PrimaryReplica' } ).NodeName
                 PrimaryReplicaInstanceName = ( $AllNodes | Where-Object { $_.Role -eq 'PrimaryReplica' } ).SQLInstanceName
+                DependsOn                  = '[SqlAlwaysOnService]EnableHADR'
             }
         }
 
