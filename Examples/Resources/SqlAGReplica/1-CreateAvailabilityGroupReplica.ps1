cdd68407650bc60282470377cf2e425bb30ae7b8
Johan Ljunggren <johan.ljunggren@shamn.se>
2018-09-05 10:11:15 +0200

    Fix duplicate resource instance

--- Examples/Resources/SqlAGReplica/1-CreateAvailabilityGroupReplica.ps1
+++ Examples/Resources/SqlAGReplica/1-CreateAvailabilityGroupReplica.ps1
@@ -83,14 +83,6 @@ Configuration Example
             PsDscRunAsCredential = $SqlAdministratorCredential
         }
 
-        SqlAlwaysOnService EnableHADR
-        {
-            Ensure               = 'Present'
-            InstanceName         = $Node.InstanceName
-            ServerName           = $Node.NodeName
-            PsDscRunAsCredential = $SqlAdministratorCredential
-        }
-
         if ( $Node.Role -eq 'PrimaryReplica' )
         {
             # Create the availability group on the instance tagged as the primary replica
