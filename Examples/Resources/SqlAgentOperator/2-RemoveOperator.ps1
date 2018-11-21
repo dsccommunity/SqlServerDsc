0cd492aec294ce19a1f580b7eaed62930da518c6
jpomfret <jpomfret7@gmail.com>
2018-11-21 11:25:26 -0500

    add examples

---
+++ Examples/Resources/SqlAgentOperator/2-RemoveOperator.ps1
@@ -0,0 +1,25 @@
+<#
+.EXAMPLE
+    This example shows how to ensure that the SQL Agent Operator
+    DbaTeam does not exist.
+#>
+
+Configuration Example
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.PSCredential]
+        $SqlAdministratorCredential
+    )
+
+    Import-DscResource -ModuleName SqlServerDsc
+
+    node localhost {
+        SqlAgentOperator Remove_DbaTeam {
+            Ensure               = 'Absent'
+            Name                 = 'DbaTeam'
+            ServerName           = 'TestServer'
+            InstanceName         = 'MSSQLServer'
+        }
+    }
+}
