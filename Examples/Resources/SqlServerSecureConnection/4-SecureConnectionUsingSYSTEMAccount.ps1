9dc52427507528db61b9e2af843f3bb3a501f47a
james-flynn-ie <29207704+james-flynn-ie@users.noreply.github.com>
2019-04-08 20:02:28 +0100

    Renaming example file for clarity

--- Examples/Resources/SqlServerSecureConnection/4-SecureConnectionUsingSYSTEMAccount.ps1
+++ Examples/Resources/SqlServerSecureConnection/4-SecureConnectionUsingSYSTEMAccount.ps1
@@ -9,7 +9,7 @@ Configuration Example
     Import-DscResource -ModuleName SqlServerDsc
 
     node localhost {
-        SqlServerSecureConnection SecureConnectionSYSTEMAccount
+        SqlServerSecureConnection SecureConnectionUsingSYSTEMAccount
         {
             InstanceName    = 'MSSQLSERVER'
             Thumbprint      = 'fb0b82c94b80da26cf0b86f10ec0c50ae7864a2c'
