f6ed1b02bdc4c95c81dde2683fe1421285ed9d1c
Johan Ljunggren <johan.ljunggren@shamn.se>
2019-12-12 17:15:15 +0100

    Add temp step to configure LCM

--- Tests/Integration/MSFT_SqlSetup.Integration.Tests.ps1
+++ Tests/Integration/MSFT_SqlSetup.Integration.Tests.ps1
@@ -6,14 +6,91 @@ if (Test-SkipContinuousIntegrationTask -Type 'Integration' -Category @('Integrat
 }
 
 <#
+    .SYNOPSIS
+        This command will initialize the Local Configuration Manager. It's
+        meant to be used before running tests.
+    .PARAMETER DisableConsistency
+        This will switch off monitoring (consistency) for the Local Configuration
+        Manager (LCM), setting ConfigurationMode to 'ApplyOnly', on the node
+        running tests.
+    .PARAMETER Encrypt
+        This will switch on encryption for the Local Configuration
+        Manager (LCM), setting CertificateId to the thumbprint stored in
+        $env:DscCertificateThumbprint, on the node running tests.
+        When using this parameter any configuration used for an integration
+        test must have CertificateFile pointing to path stored in
+        $env:DscPublicCertificatePath.
+#>
+function Initialize-LocalConfigurationManager
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter()]
+        [Switch]
+        $DisableConsistency,
+
+        [Parameter()]
+        [Switch]
+        $Encrypt
+    )
+
+    $disableConsistencyMofPath = Join-Path -Path $env:temp -ChildPath 'LCMConfiguration'
+    if (-not (Test-Path -Path $disableConsistencyMofPath))
+    {
+        $null = New-Item -Path $disableConsistencyMofPath -ItemType Directory -Force
+    }
+
+    # Start of the metadata configuration
+    $configurationMetadata = '
+        Configuration LocalConfigurationManagerConfiguration
+        {
+            LocalConfigurationManager
+            {
+    '
+
+    if ($DisableConsistency.IsPresent)
+    {
+        Write-Verbose -Message 'Setting Local Configuration Manager property ConfigurationMode to ''ApplyOnly'', disabling consistency check.' -Verbose
+        # Have LCM Apply only once.
+        $configurationMetadata += '
+            ConfigurationMode = ''ApplyOnly''
+        '
+    }
+
+    if ($Encrypt.IsPresent)
+    {
+        Write-Verbose -Message ('Setting Local Configuration Manager property CertificateId to ''{0}'', enabling decryption of credentials.' -f $env:DscCertificateThumbprint) -Verbose
+        # Should use encryption.
+        $configurationMetadata += ('
+            CertificateId = ''{0}''
+        ' -f $env:DscCertificateThumbprint)
+    }
+
+    # End of the metadata configuration
+    $configurationMetadata += '
+            }
+        }
+    '
+
+    Invoke-Command -ScriptBlock ([scriptblock]::Create($configurationMetadata)) -NoNewScope
+
+    LocalConfigurationManagerConfiguration -OutputPath $disableConsistencyMofPath
+
+    Set-DscLocalConfigurationManager -Path $disableConsistencyMofPath -Force -Verbose
+    $null = Remove-Item -LiteralPath $disableConsistencyMofPath -Recurse -Force -Confirm:$false
+}
+
+<#
     Prerequisites modules needed for integration tests
     (uses force because the repository is not trusted)
 #>
-Install-Module -Name 'PSDscResources' -Force -Verbose
-Install-Module -Name 'StorageDsc' -Force -Verbose
-Install-Module -Name 'NetworkingDsc' -Force -Verbose
+Install-Module -Name 'PSDscResources' -Force
+Install-Module -Name 'StorageDsc' -Force
+Install-Module -Name 'NetworkingDsc' -Force
 
 $null = New-DscSelfSignedCertificate -Verbose
+Initialize-LocalConfigurationManager -Encrypt:$true -DisableConsistency:$DisableConsistency
 
 $script:dscModuleName = 'SqlServerDsc'
 $script:dscResourceFriendlyName = 'SqlSetup'
