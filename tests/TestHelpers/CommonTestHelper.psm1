a6112afadd0763104b516312cdbf3dbae7f09e64
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-12-15 19:39:29 +0000

    Added 'Clear' switch into 'Wait-ForIdleLcm', test helper method.

--- tests/TestHelpers/CommonTestHelper.psm1
+++ tests/TestHelpers/CommonTestHelper.psm1
@@ -388,6 +388,9 @@ function Test-ContinuousIntegrationTaskCategory
     .SYNOPSIS
         Waits for LCM to become idle.
 
+    .PARAMETER Clear
+        If specified, the LCM will also be cleared of DSC configurations.
+
     .NOTES
         Used in integration test where integration tests run to quickly before
         LCM have time to cool down.
@@ -395,13 +398,23 @@ function Test-ContinuousIntegrationTaskCategory
 function Wait-ForIdleLcm
 {
     [CmdletBinding()]
-    param ()
+    param
+    (
+        [Parameter()]
+        [System.Management.Automation.SwitchParameter]
+        $Clear
+    )
 
     while ((Get-DscLocalConfigurationManager).LCMState -ne 'Idle')
     {
         Write-Verbose -Message 'Waiting for the LCM to become idle'
 
         Start-Sleep -Seconds 2
+    }
+
+    if ($Clear)
+    {
+        Clear-DscLcmConfiguration
     }
 }
 
