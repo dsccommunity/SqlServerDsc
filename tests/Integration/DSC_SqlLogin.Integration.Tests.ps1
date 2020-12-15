61ec7c4c52d55289f3abe4d3c539f525f2e10f95
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-12-15 18:11:24 +0000

    Corrected 'SqlLogin', 'LoginMustChangePassword' test assertion within 'UpdateLoginDscUser4' configuration, integration tests.

--- tests/Integration/DSC_SqlLogin.Integration.Tests.ps1
+++ tests/Integration/DSC_SqlLogin.Integration.Tests.ps1
@@ -353,7 +353,7 @@ try
                 $resourceCurrentState.Name | Should -Be $ConfigurationData.AllNodes.DscUser4Name
                 $resourceCurrentState.LoginType | Should -Be $ConfigurationData.AllNodes.DscUser4Type
                 $resourceCurrentState.Disabled | Should -Be $false
-                $resourceCurrentState.LoginMustChangePassword | Should -Be $true
+                $resourceCurrentState.LoginMustChangePassword | Should -Be $false # Left the same as this cannot be updated
                 $resourceCurrentState.LoginPasswordExpirationEnabled | Should -Be $false
                 $resourceCurrentState.LoginPasswordPolicyEnforced | Should -Be $false
             }
