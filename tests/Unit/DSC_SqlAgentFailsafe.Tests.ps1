fb444ccab4496c5f2f7db57b38193da2a09b0c00
Johan Ljunggren <johan.ljunggren@shamn.se>
2021-08-19 11:08:18 +0200

    Fix to control that build.ps1 has ran

--- tests/Unit/DSC_SqlAgentFailsafe.Tests.ps1
+++ tests/Unit/DSC_SqlAgentFailsafe.Tests.ps1
@@ -3,10 +3,7 @@
         Unit test for DSC_SqlAgentFailsafe DSC resource.
 #>
 
-BeforeAll {
-    $script:dscModuleName = 'SqlServerDsc'
-    $script:dscResourceName = 'DSC_SqlAgentFailsafe'
-
+BeforeDiscovery {
     try
     {
         Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
@@ -15,6 +12,11 @@ BeforeAll {
     {
         throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
     }
+}
+
+BeforeAll {
+    $script:dscModuleName = 'SqlServerDsc'
+    $script:dscResourceName = 'DSC_SqlAgentFailsafe'
 
     $script:testEnvironment = Initialize-TestEnvironment `
         -DSCModuleName $script:dscModuleName `
