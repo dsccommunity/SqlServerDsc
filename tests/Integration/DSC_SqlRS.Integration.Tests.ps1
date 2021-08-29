abd6bf0e0367a55556f5c4e2218c924d5b63cd21
Johan Ljunggren <johan.ljunggren@shamn.se>
2021-08-29 16:57:07 +0200

    add test

--- tests/Integration/DSC_SqlRS.Integration.Tests.ps1
+++ tests/Integration/DSC_SqlRS.Integration.Tests.ps1
@@ -332,6 +332,36 @@ try
 
                 $webRequestStatusCode | Should -BeExactly 200
             }
+
+            It 'Should be able to access the Reports site without any error' {
+                if($script:sqlVersion -in @('140', '150'))
+                {
+                    # SSRS 2017 and 2019 do not support multiple instances
+                    $reportsUri = 'http://{0}/Reports' -f $env:COMPUTERNAME
+                }
+                else
+                {
+                    $reportsUri = 'http://{0}/Reports_{1}' -f $env:COMPUTERNAME, $ConfigurationData.AllNodes.InstanceName
+                }
+
+                try
+                {
+                    $webRequestReportServer = Invoke-WebRequest -Uri $reportsUri -UseDefaultCredentials
+                    # if the request finishes successfully this should return status code 200.
+                    $webRequestStatusCode = $webRequestReportServer.StatusCode -as [int]
+                }
+                catch
+                {
+                    <#
+                        If the request generated an exception i.e. "HTTP Error 503. The service is unavailable."
+                        we can pull the status code from the Exception.Response property.
+                    #>
+                    $webRequestResponse = $_.Exception.Response
+                    $webRequestStatusCode = $webRequestResponse.StatusCode -as [int]
+                }
+
+                $webRequestStatusCode | Should -BeExactly 200
+            }
         }
 
         $configurationName = "$($script:dscResourceName)_StopReportingServicesInstance_Config"
