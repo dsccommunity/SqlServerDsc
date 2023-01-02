c7f9a95115db6148cccde43ef1cbf037dc7062ce
Johan Ljunggren <johan.ljunggren@shamn.se>
2023-01-02 19:19:23 +0100

    SqlServerDsc: Add QA tests for commands

--- tests/QA/ScriptAnalyzer.Tests.ps1
+++ tests/QA/ScriptAnalyzer.Tests.ps1
@@ -10,8 +10,30 @@
         repository DscResource.Test is resolved this should not be needed. See issue
         https://github.com/dsccommunity/DscResource.Test/issues/100.
 #>
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param ()
 
 BeforeDiscovery {
+    try
+    {
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 2>&1 4>&1 5>&1 6>&1 > $null
+            }
+
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+        }
+    }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
+
     <#
         Need to add the SMO stub classes so that the analyzer rule 'UseSyntacticallyCorrectExamples'
         (from Indented.ScriptAnalyzerRules) can properly parse parameters that uses SMO types,
