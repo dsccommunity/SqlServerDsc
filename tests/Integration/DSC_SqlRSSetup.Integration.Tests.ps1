c2087ce5ed6e8ee4049b791287532ee0ff261395
Johan Ljunggren <johan.ljunggren@shamn.se>
2023-02-07 19:23:10 +0100

    Fix version

--- tests/Integration/DSC_SqlRSSetup.Integration.Tests.ps1
+++ tests/Integration/DSC_SqlRSSetup.Integration.Tests.ps1
@@ -50,9 +50,9 @@ BeforeAll {
         {
             <#
                 The version below is what the MS download page said, but the .exe is
-                reporting 15.0.7842.32355 when used in the integration test.
+                reporting 15.0.8434.2956 when used in the integration test.
             #>
-            $script:mockSourceMediaDisplayName = 'Microsoft SQL Server 2019 Reporting Services (15.0.1102.911 - 6/24/2021)'
+            $script:mockSourceMediaDisplayName = 'Microsoft SQL Server 2019 Reporting Services (15.0.1102.1047 - 2/6/2023)'
             $script:mockSourceMediaUrl = 'https://download.microsoft.com/download/1/a/a/1aaa9177-3578-4931-b8f3-373b24f63342/SQLServerReportingServices.exe'
         }
 
