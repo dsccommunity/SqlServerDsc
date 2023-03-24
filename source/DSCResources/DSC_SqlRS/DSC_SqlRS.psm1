67acf0c90674c4fd88ecd18a04495f6884c282ee
Johan Ljunggren <johan.ljunggren@shamn.se>
2023-03-24 15:39:13 +0100

    DEBUG

--- source/DSCResources/DSC_SqlRS/DSC_SqlRS.psm1
+++ source/DSCResources/DSC_SqlRS/DSC_SqlRS.psm1
@@ -477,10 +477,10 @@ function Set-TargetResource
                 ServerInstance = $reportingServicesConnection
             }
 
-            Write-Verbose -Message ('PSModulePath: {0}' -f $env:PSModulePath) -Verbose
-            Write-Verbose -Message ("Loaded modules:`r`n{0}" -f (Get-Module | Out-String )) -Verbose
-            Write-Verbose -Message ("Available SqlServer modules:`r`n{0}" -f (Get-Module -Name @('SqlServer', 'SQLPS') -ListAvailable | Out-String )) -Verbose
-
+            Write-Verbose -Message ("PSModulePath: {0}`r`n" -f $env:PSModulePath) -Verbose
+            Write-Verbose -Message ("Loaded modules:`r`n{0}`r`n" -f (Get-Module | Out-String )) -Verbose
+            Write-Verbose -Message ("Available SqlServer modules:`r`n{0}`r`n" -f (Get-Module -Name @('SqlServer', 'SQLPS') -ListAvailable | Out-String )) -Verbose
+            Write-Verbose -Message ("Invoke-SqlCmd:`r`n{0}`r`n" -f (Get-Command -Name 'Invoke-SqlCmd' -ErrorAction 'SilentlyContinue' | Out-String )) -Verbose
 
             if ($PSBoundParameters.ContainsKey('Encrypt'))
             {
