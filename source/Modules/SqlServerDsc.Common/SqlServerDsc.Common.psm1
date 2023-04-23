44c654a2ebb93a3728c2f3e1a9152f3e4ebc4b76
Johan Ljunggren <johlju@users.noreply.github.com>
2023-04-23 15:45:28 +0200

    SqlRS: Using `Invoke-SqlDscQuery` (#1919)
    - SqlServerDsc.Common
      - `Connect-SQL`.
        - Add new parameter `Encrypt`.
    - `Connect-SqlDscDatabaseEngine`
      - Add new parameter `Encrypt`.
    - `Invoke-SqlDscQuery`
      - Add new parameter `Encrypt`.
      - Now shows the correct instance name when called using a server object (issue #1918).
    - SqlRS
      - Now uses the command `Invoke-SqlDscQuery` instead of `Invoke-SqlCmd` (issue #1917).
      - The parameter `Encrypt` has changed so that `Mandatory` or `Strict`
        will turn on encryption when connecting to the database instance.

--- source/Modules/SqlServerDsc.Common/SqlServerDsc.Common.psm1
+++ source/Modules/SqlServerDsc.Common/SqlServerDsc.Common.psm1
@@ -447,6 +447,9 @@ function Start-SqlSetupProcess
     .PARAMETER StatementTimeout
         Set the query StatementTimeout in seconds. Default 600 seconds (10 minutes).
 
+    .PARAMETER Encrypt
+        Specifies if encryption should be used.
+
     .EXAMPLE
         Connect-SQL
 
@@ -493,7 +496,11 @@ function Connect-SQL
         [Parameter()]
         [ValidateNotNull()]
         [System.Int32]
-        $StatementTimeout = 600
+        $StatementTimeout = 600,
+
+        [Parameter()]
+        [System.Management.Automation.SwitchParameter]
+        $Encrypt
     )
 
     Import-SqlDscPreferredModule
@@ -512,6 +519,11 @@ function Connect-SQL
     $sqlConnectionContext.ServerInstance = $databaseEngineInstance
     $sqlConnectionContext.StatementTimeout = $StatementTimeout
     $sqlConnectionContext.ApplicationName = 'SqlServerDsc'
+
+    if ($Encrypt.IsPresent)
+    {
+        $sqlConnectionContext.EncryptConnection = $true
+    }
 
     if ($PSCmdlet.ParameterSetName -eq 'SqlServer')
     {
