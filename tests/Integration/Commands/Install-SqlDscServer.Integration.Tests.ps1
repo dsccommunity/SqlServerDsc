f6e722dfd413eba081634c9ccf78b9ff02494093
Johan Ljunggren <johan.ljunggren@shamn.se>
2024-05-09 17:03:06 +0200

    Fix integ test

--- tests/Integration/Commands/Install-SqlDscServer.Integration.Tests.ps1
+++ tests/Integration/Commands/Install-SqlDscServer.Integration.Tests.ps1
@@ -38,6 +38,7 @@ Describe 'Install-SqlDscServer' -Tag @('Integration_SQL2016', 'Integration_SQL20
                         MediaPath = $env:IsoDrivePath # Set by the prerequisites tests
                         Verbose = $true
                         ErrorAction = 'Stop'
+                        Force = $true
                     }
 
                     Install-SqlDscServer @installSqlDscServerParameters
