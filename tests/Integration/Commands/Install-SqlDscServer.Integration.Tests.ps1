3de6a477436f498ba792ce5e34e77f798a92a2f2
Johan Ljunggren <johan.ljunggren@shamn.se>
2024-05-09 22:00:08 +0200

    Fix integ test

--- tests/Integration/Commands/Install-SqlDscServer.Integration.Tests.ps1
+++ tests/Integration/Commands/Install-SqlDscServer.Integration.Tests.ps1
@@ -28,22 +28,32 @@ Describe 'Install-SqlDscServer' -Tag @('Integration_SQL2016', 'Integration_SQL20
         Context 'When installing database engine default instance' {
             It 'Should run the command without throwing' {
                 {
-                    # TODO: Add additional properties, for example service accounts, to the splatting parameters.
                     # Set splatting parameters for Install-SqlDscServer
                     $installSqlDscServerParameters = @{
-                        Install              = $true
-                        AcceptLicensingTerms = $true
-                        InstanceName         = 'MSSQLSERVER'
-                        Features             = 'SQLENGINE'
-                        SqlSysAdminAccounts  = @(
+                        Install               = $true
+                        AcceptLicensingTerms  = $true
+                        InstanceName          = 'MSSQLSERVER'
+                        Features              = 'SQLENGINE'
+                        SqlSysAdminAccounts   = @(
                             ('{0}\SqlAdmin' -f (Get-ComputerName))
                         )
-                        MediaPath            = $env:IsoDrivePath # Set by the prerequisites tests
-                        Verbose              = $true
-                        ErrorAction          = 'Stop'
-                        Force                = $true
+                        SqlSvcAccount         = '{0}\svc-SqlPrimary' -f (Get-ComputerName)
+                        SqlSvcPassword        = ConvertTo-SecureString -String 'yig-C^Equ3' -AsPlainText -Force
+                        SqlSvcStartupType     = 'Automatic'
+                        AgtSvcAccount         = '{0}\svc-SqlAgentPri' -f (Get-ComputerName)
+                        AgtSvcPassword        = ConvertTo-SecureString -String 'yig-C^Equ3' -AsPlainText -Force
+                        AgtSvcStartupType     = 'Automatic'
+                        BrowserSvcStartupType = 'Automatic'
+                        SecurityMode          = 'SQL'
+                        SAPwd                 = ConvertTo-SecureString -String 'P@ssw0rd1' -AsPlainText -Force
+                        SqlCollation          = 'Finnish_Swedish_CI_AS'
+                        MediaPath             = $env:IsoDrivePath # Already set by the prerequisites tests
+                        Verbose               = $true
+                        ErrorAction           = 'Stop'
+                        Force                 = $true
                     }
 
+                    # TODO: Should run command as SqlInstall user.
                     Install-SqlDscServer @installSqlDscServerParameters
                 } | Should -Not -Throw
             }
