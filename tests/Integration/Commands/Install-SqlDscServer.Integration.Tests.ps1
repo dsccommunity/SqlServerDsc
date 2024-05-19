bc814e88c5e532f5e8f1458ec85293a672a05e46
Johan Ljunggren <johan.ljunggren@shamn.se>
2024-05-19 14:52:12 +0200

    Try enable named instance

--- tests/Integration/Commands/Install-SqlDscServer.Integration.Tests.ps1
+++ tests/Integration/Commands/Install-SqlDscServer.Integration.Tests.ps1
@@ -27,8 +27,8 @@ Describe 'Install-SqlDscServer' -Tag @('Integration_SQL2016', 'Integration_SQL20
     BeforeAll {
         Write-Verbose -Message ('Running integration test as user ''{0}''.' -f $env:UserName) -Verbose
 
-    #     # Get the built SqlServerDsc module path.
-    #     $modulePath = Split-Path -Parent -Path (Get-Module -name SqlServerDsc -ListAvailable).ModuleBase
+        # Get the built SqlServerDsc module path.
+        $modulePath = Split-Path -Parent -Path (Get-Module -name SqlServerDsc -ListAvailable).ModuleBase
     }
 
     Context 'When using Install parameter set' {
@@ -155,49 +155,139 @@ Describe 'Install-SqlDscServer' -Tag @('Integration_SQL2016', 'Integration_SQL20
             }
         }
 
-        # Context 'Output the Summary.txt log file' {
-        #     BeforeAll {
-        #         <#
-        #             .SYNOPSIS
-        #                 This function will output the Setup Bootstrap Summary.txt log file.
+        Context 'When installing database engine named instance' {
+            It 'Should run the command without throwing' {
+                {
+                    <#
+                        Fails with the following error message:
 
-        #             .DESCRIPTION
-        #                 This function will output the Summary.txt log file, this is to be
-        #                 able to debug any problems that potentially occurred during setup.
-        #                 This will pick up the newest Summary.txt log file, so any
-        #                 other log files will be ignored (AppVeyor build worker has
-        #                 SQL Server instances installed by default).
-        #                 This code is meant to work regardless what SQL Server
-        #                 major version is used for the integration test.
-        #         #>
-        #         function Show-SqlBootstrapLog
-        #         {
-        #             [CmdletBinding()]
-        #             param
-        #             (
-        #             )
+                        VERBOSE:   Exit code (Decimal):           -2068774911
+                        VERBOSE:   Exit facility code:            1201
+                        VERBOSE:   Exit error code:               1
+                        VERBOSE:   Exit message:                  There was an error generating the XML document.
 
-        #             $summaryLogPath = Get-ChildItem -Path 'C:\Program Files\Microsoft SQL Server\**\Setup Bootstrap\Log\Summary.txt' |
-        #                 Sort-Object -Property LastWriteTime -Descending |
-        #                 Select-Object -First 1
+                        Searches points to a permission issue, but the user has been
+                        granted the local administrator permissions. But code be
+                        Searches also points to user right SeEnableDelegationPrivilege
+                        which was not evaluated if it was set correctly or even needed.
+                    #>
+                    $installScriptBlock = {
+                        param
+                        (
+                            [Parameter(Mandatory = $true)]
+                            [System.String]
+                            $IsoDrivePath,
 
-        #             $summaryLog = Get-Content $summaryLogPath
+                            [Parameter(Mandatory = $true)]
+                            [System.String]
+                            $ComputerName,
 
-        #             Write-Verbose -Message $('-' * 80) -Verbose
-        #             Write-Verbose -Message 'Summary.txt' -Verbose
-        #             Write-Verbose -Message $('-' * 80) -Verbose
+                            [Parameter(Mandatory = $true)]
+                            [System.String]
+                            $ModulePath
+                        )
 
-        #             $summaryLog | ForEach-Object -Process {
-        #                 Write-Verbose $_ -Verbose
-        #             }
+                        Write-Verbose -Message ('Running install as user ''{0}''.' -f $env:UserName) -Verbose
 
-        #             Write-Verbose -Message $('-' * 80) -Verbose
-        #         }
-        #     }
+                        Import-Module -Name $ModulePath -Force -ErrorAction 'Stop'
 
-        #     It 'Should output the Summary.txt log file' {
-        #         Show-SqlBootstrapLog
-        #     }
-        # }
+                        # Set splatting parameters for Install-SqlDscServer
+                        $installSqlDscServerParameters = @{
+                            Install               = $true
+                            AcceptLicensingTerms  = $true
+                            InstanceName          = 'DSCSQLTEST' # cSpell: disable-line
+                            Features              = 'SQLENGINE'
+                            SqlSysAdminAccounts   = @(
+                                ('{0}\SqlAdmin' -f $ComputerName)
+                            )
+                            SqlSvcAccount         = '{0}\svc-SqlPrimary' -f $ComputerName
+                            SqlSvcPassword        = ConvertTo-SecureString -String 'yig-C^Equ3' -AsPlainText -Force
+                            SqlSvcStartupType     = 'Automatic'
+                            AgtSvcAccount         = '{0}\svc-SqlAgentPri' -f $ComputerName
+                            AgtSvcPassword        = ConvertTo-SecureString -String 'yig-C^Equ3' -AsPlainText -Force
+                            AgtSvcStartupType     = 'Automatic'
+                            BrowserSvcStartupType = 'Automatic'
+                            SecurityMode          = 'SQL'
+                            SAPwd                 = ConvertTo-SecureString -String 'P@ssw0rd1' -AsPlainText -Force
+                            SqlCollation          = 'Finnish_Swedish_CI_AS'
+                            InstallSharedDir      = 'C:\Program Files\Microsoft SQL Server'
+                            InstallSharedWOWDir   = 'C:\Program Files (x86)\Microsoft SQL Server'
+                            NpEnabled             = $true
+                            TcpEnabled            = $true
+                            MediaPath             = $IsoDrivePath
+                            Verbose               = $true
+                            ErrorAction           = 'Stop'
+                            Force                 = $true
+                        }
+
+                        Install-SqlDscServer @installSqlDscServerParameters
+                    }
+
+                    $invokeCommandUsername = 'SqlInstall' # Using computer name as NetBIOS name throw exception.
+                    $invokeCommandPassword = ConvertTo-SecureString -String 'P@ssw0rd1' -AsPlainText -Force
+                    $invokeCommandCredential = New-Object System.Management.Automation.PSCredential ($invokeCommandUsername, $invokeCommandPassword)
+
+                    # Runs command as SqlInstall user.
+                    Invoke-Command -ComputerName 'localhost' -Credential $invokeCommandCredential -ScriptBlock $installScriptBlock -ArgumentList @(
+                        $env:IsoDrivePath, # Already set by the prerequisites tests
+                        (Get-ComputerName),
+                        $modulePath
+                    )
+                } | Should -Not -Throw
+            }
+
+            It 'Should have installed the SQL Server database engine' {
+                # Validate the SQL Server installation
+                $sqlServerService = Get-Service -Name 'SQL Server (DSCSQLTEST)' # cSpell: disable-line
+
+                $sqlServerService | Should -Not -BeNullOrEmpty
+                $sqlServerService.Status | Should -Be 'Running'
+            }
+        }
+
+        Context 'Output the Summary.txt log file' {
+            BeforeAll {
+                <#
+                    .SYNOPSIS
+                        This function will output the Setup Bootstrap Summary.txt log file.
+
+                    .DESCRIPTION
+                        This function will output the Summary.txt log file, this is to be
+                        able to debug any problems that potentially occurred during setup.
+                        This will pick up the newest Summary.txt log file, so any
+                        other log files will be ignored (AppVeyor build worker has
+                        SQL Server instances installed by default).
+                        This code is meant to work regardless what SQL Server
+                        major version is used for the integration test.
+                #>
+                function Show-SqlBootstrapLog
+                {
+                    [CmdletBinding()]
+                    param
+                    (
+                    )
+
+                    $summaryLogPath = Get-ChildItem -Path 'C:\Program Files\Microsoft SQL Server\**\Setup Bootstrap\Log\Summary.txt' |
+                        Sort-Object -Property LastWriteTime -Descending |
+                        Select-Object -First 1
+
+                    $summaryLog = Get-Content $summaryLogPath
+
+                    Write-Verbose -Message $('-' * 80) -Verbose
+                    Write-Verbose -Message 'Summary.txt' -Verbose
+                    Write-Verbose -Message $('-' * 80) -Verbose
+
+                    $summaryLog | ForEach-Object -Process {
+                        Write-Verbose $_ -Verbose
+                    }
+
+                    Write-Verbose -Message $('-' * 80) -Verbose
+                }
+            }
+
+            It 'Should output the Summary.txt log file' {
+                Show-SqlBootstrapLog
+            }
+        }
     }
 }
