e6b83a022466311d2aec835b73686a88644ce6f1
Saravana Raj Balasubramanian <bsaravana@athenahealth.com>
2024-09-04 19:05:21 +0530

    version 16

--- tests/Unit/DSC_SqlSetup.Tests.ps1
+++ tests/Unit/DSC_SqlSetup.Tests.ps1
@@ -2763,7 +2763,7 @@ Describe 'SqlSetup\Set-TargetResource' -Tag 'Set' {
                         }
                         elseif ($MockSqlMajorVersion -in ('16'))
                         {
-                            $mockSetTargetResourceParameters.Features = $mockSetTargetResourceParameters.Features -replace ',Conn,RS', ''
+                            $mockSetTargetResourceParameters.Features = $mockSetTargetResourceParameters.Features -replace ',Conn', ''
                         }
 
                         { Set-TargetResource @mockSetTargetResourceParameters } | Should -Not -Throw
