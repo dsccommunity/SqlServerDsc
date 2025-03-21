d12aa8522ab6929197b08cd59444a228f387c7a3
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-03-21 11:03:27 +0100

    Add localized resource strings for SqlRSSetup, improve parameter descriptions, add unit tests

---
+++ source/en-US/SqlRSSetup.strings.psd1
@@ -0,0 +1,30 @@
+<#
+    .SYNOPSIS
+        The localized resource strings in English (en-US) for the
+        resource SqlRSSetup.
+#>
+
+# cSpell:ignore SSRS PBIRS
+ConvertFrom-StringData @'
+    ## Strings overrides for the ResourceBase's default strings.
+    # None
+
+    ## Strings directly used by the derived class SqlRSSetup.
+    Evaluating = Evaluating SQL Reporting Services setup for instance '{0}'.
+    Instance_NotInstalled = Instance '{0}' is not installed.
+    Instance_Installed = Instance '{0}' is installed.
+    AcceptLicensingTerms_Required = AcceptLicensingTerms must be set for Install and Repair operations.
+    MediaPath_Invalid = The media path '{0}' does not exist.
+    EditionOrProductKeyMissing = Neither the parameters Edition or ProductKey was specified.
+    EditionUpgrade_RequiresKeyOrEdition = EditionUpgrade requires either ProductKey or Edition to be specified.
+    LogPath_ParentMissing = The parent directory '{0}' for LogPath does not exist.
+    InstallFolder_ParentMissing = The parent directory '{0}' for InstallFolder does not exist.
+    Installing_ReportingServices = Installing SQL Server Reporting Services.
+    Installing_PowerBIReportServer = Installing Power BI Report Server.
+    Repairing_ReportingServices = Repairing SQL Server Reporting Services.
+    Repairing_PowerBIReportServer = Repairing Power BI Report Server.
+    Uninstalling_ReportingServices = Uninstalling SQL Server Reporting Services.
+    Uninstalling_PowerBIReportServer = Uninstalling Power BI Report Server.
+    NotDesiredProductVersion = The product version '{0}' is not the desired for the instance '{1}'. Desired version in executable: '{2}'.
+    InstanceName_Invalid = The instance name '{0}' is invalid. Only one of the supported instance name can be used, either SSRS or PBIRS depending on what setup executable is used.
+'@
---
+++ tests/Unit/Classes/SqlRSSetup.Tests.ps1
@@ -0,0 +1,260 @@
+<#
+    .SYNOPSIS
+        Unit test for SqlRSSetup DSC resource.
+#>
+
+# Suppressing this rule because Script Analyzer does not understand Pester's syntax.
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+param ()
+
+BeforeDiscovery {
+    try
+    {
+        if (-not (Get-Module -Name 'DscResource.Test'))
+        {
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../../build.ps1" -Tasks 'noop' 2>&1 4>&1 5>&1 6>&1 > $null
+            }
+
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+        }
+    }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+    }
+}
+
+BeforeAll {
+    $script:dscModuleName = 'SqlServerDsc'
+
+    $env:SqlServerDscCI = $true
+
+    Import-Module -Name $script:dscModuleName
+
+    $PSDefaultParameterValues['InModuleScope:ModuleName'] = $script:dscModuleName
+    $PSDefaultParameterValues['Mock:ModuleName'] = $script:dscModuleName
+    $PSDefaultParameterValues['Should:ModuleName'] = $script:dscModuleName
+}
+
+AfterAll {
+    $PSDefaultParameterValues.Remove('InModuleScope:ModuleName')
+    $PSDefaultParameterValues.Remove('Mock:ModuleName')
+    $PSDefaultParameterValues.Remove('Should:ModuleName')
+
+    # Unload the module being tested so that it doesn't impact any other tests.
+    Get-Module -Name $script:dscModuleName -All | Remove-Module -Force
+
+    Remove-Item -Path 'env:SqlServerDscCI'
+}
+
+Describe 'SqlRSSetup' {
+    Context 'When class is instantiated' {
+        It 'Should not throw an exception' {
+            InModuleScope -ScriptBlock {
+                { [SqlRSSetup]::new() } | Should -Not -Throw
+            }
+        }
+
+        It 'Should have a default or empty constructor' {
+            InModuleScope -ScriptBlock {
+                $instance = [SqlRSSetup]::new()
+                $instance | Should -Not -BeNullOrEmpty
+            }
+        }
+
+        It 'Should be the correct type' {
+            InModuleScope -ScriptBlock {
+                $instance = [SqlRSSetup]::new()
+                $instance.GetType().Name | Should -Be 'SqlRSSetup'
+            }
+        }
+    }
+}
+
+Describe 'SqlRSSetup\Get()' -Tag 'Get' {
+    Context 'When the system is in the desired state' {
+        Context 'When the instance is installed' {
+            BeforeAll {
+                InModuleScope -ScriptBlock {
+                    $script:mockSqlRSSetupInstance = [SqlRSSetup] @{
+                        InstanceName = 'SSRS'
+                        Edition      = 'Developer'
+                        Action       = 'Install'
+                    }
+
+                    <#
+                        This mocks the method GetCurrentState().
+
+                        Method Get() will call the base method Get() which will
+                        call back to the derived class method GetCurrentState()
+                        to get the result to return from the derived method Get().
+                    #>
+                    $script:mockSqlRSSetupInstance |
+                        Add-Member -Force -MemberType 'ScriptMethod' -Name 'GetCurrentState' -Value {
+                            return [System.Collections.Hashtable] @{
+                                InstanceName = 'SSRS'
+                            }
+                        } -PassThru |
+                        Add-Member -Force -MemberType 'ScriptMethod' -Name 'AssertProperties' -Value {
+                            return
+                        } -PassThru |
+                        Add-Member -Force -MemberType 'ScriptMethod' -Name 'NormalizeProperties' -Value {
+                            return
+                        }
+                }
+            }
+
+            It 'Should return the correct values' {
+                InModuleScope -ScriptBlock {
+                    $currentState = $script:mockSqlRSSetupInstance.Get()
+
+                    $currentState.InstanceName | Should -Be 'SSRS'
+                    $currentState.Timeout | Should -Be 7200
+                    $currentState.Action | Should -BeNullOrEmpty
+                    $currentState.AcceptLicensingTerms | Should -BeFalse
+                    $currentState.MediaPath | Should -BeNullOrEmpty
+                    $currentState.ProductKey | Should -BeNullOrEmpty
+                    $currentState.EditionUpgrade | Should -BeNullOrEmpty
+                    $currentState.Edition | Should -BeNullOrEmpty
+                    $currentState.LogPath | Should -BeNullOrEmpty
+                    $currentState.InstallFolder | Should -BeNullOrEmpty
+                    $currentState.SuppressRestart | Should -BeFalse
+                    $currentState.ForceRestart | Should -BeFalse
+                    $currentState.VersionUpgrade | Should -BeNullOrEmpty
+                    $currentState.ProductVersion | Should -BeNullOrEmpty
+                }
+            }
+        }
+    }
+
+    Context 'When the system is not in the desired state' {
+        Context 'When the instance is not installed' {
+            BeforeAll {
+                InModuleScope -ScriptBlock {
+                    $script:mockSqlRSSetupInstance = [SqlRSSetup] @{
+                        InstanceName = 'SSRS'
+                        Edition      = 'Developer'
+                        Action       = 'Install'
+                    }
+
+                    <#
+                        This mocks the method GetCurrentState().
+
+                        Method Get() will call the base method Get() which will
+                        call back to the derived class method GetCurrentState()
+                        to get the result to return from the derived method Get().
+                    #>
+                    $script:mockSqlRSSetupInstance |
+                        Add-Member -Force -MemberType 'ScriptMethod' -Name 'GetCurrentState' -Value {
+                            return [System.Collections.Hashtable] @{
+                                InstanceName = $null
+                            }
+                        } -PassThru |
+                        Add-Member -Force -MemberType 'ScriptMethod' -Name 'AssertProperties' -Value {
+                            return
+                        } -PassThru |
+                        Add-Member -Force -MemberType 'ScriptMethod' -Name 'NormalizeProperties' -Value {
+                            return
+                        }
+                }
+            }
+
+            It 'Should return the correct values' {
+                InModuleScope -ScriptBlock {
+                    $currentState = $script:mockSqlRSSetupInstance.Get()
+
+                    $currentState.InstanceName | Should -BeNull
+                    $currentState.Installed | Should -BeFalse
+                    $currentState.Timeout | Should -Be 7200
+                    $currentState.Action | Should -BeNullOrEmpty
+                    $currentState.AcceptLicensingTerms | Should -BeFalse
+                    $currentState.MediaPath | Should -BeNullOrEmpty
+                    $currentState.ProductKey | Should -BeNullOrEmpty
+                    $currentState.EditionUpgrade | Should -BeNullOrEmpty
+                    $currentState.Edition | Should -BeNullOrEmpty
+                    $currentState.LogPath | Should -BeNullOrEmpty
+                    $currentState.InstallFolder | Should -BeNullOrEmpty
+                    $currentState.SuppressRestart | Should -BeFalse
+                    $currentState.ForceRestart | Should -BeFalse
+                    $currentState.VersionUpgrade | Should -BeNullOrEmpty
+                    $currentState.ProductVersion | Should -BeNullOrEmpty
+                }
+            }
+        }
+    }
+}
+
+Describe 'SqlRSSetup\Test()' -Tag 'Test' {
+    BeforeAll {
+        InModuleScope -ScriptBlock {
+            $script:mockSqlRSSetupInstance = [SqlRSSetup] @{
+                InstanceName = 'SSRS'
+                Edition      = 'Developer'
+                Action       = 'Install'
+            }
+        }
+    }
+
+    Context 'When the system is in the desired state' {
+        BeforeAll {
+            InModuleScope -ScriptBlock {
+                $script:mockSqlRSSetupInstance |
+                    # Mock method Compare() which is called by the base method Set()
+                    Add-Member -Force -MemberType 'ScriptMethod' -Name 'Compare' -Value {
+                        return $null
+                    } -PassThru |
+                    Add-Member -Force -MemberType 'ScriptMethod' -Name 'AssertProperties' -Value {
+                        return
+                    } -PassThru |
+                    Add-Member -Force -MemberType 'ScriptMethod' -Name 'NormalizeProperties' -Value {
+                        return
+                    }
+            }
+        }
+
+        It 'Should return $true' {
+            InModuleScope -ScriptBlock {
+                $script:mockSqlRSSetupInstance.Test() | Should -BeTrue
+            }
+        }
+    }
+
+    Context 'When the system is not in the desired state' {
+        BeforeAll {
+            InModuleScope -ScriptBlock {
+                $script:mockSqlRSSetupInstance |
+                    # Mock method Compare() which is called by the base method Set()
+                    Add-Member -Force -MemberType 'ScriptMethod' -Name 'Compare' -Value {
+                        <#
+                            Compare() method shall only return the properties NOT in
+                            desired state, in the format of the command Compare-DscParameterState.
+                        #>
+                        return @(
+                            @{
+                                Property      = 'Installed'
+                                ExpectedValue = $true
+                                ActualValue   = $false
+                            }
+                        )
+                    } -PassThru |
+                    Add-Member -Force -MemberType 'ScriptMethod' -Name 'AssertProperties' -Value {
+                        return
+                    } -PassThru |
+                    Add-Member -Force -MemberType 'ScriptMethod' -Name 'NormalizeProperties' -Value {
+                        return
+                    }
+            }
+        }
+
+        It 'Should return $false' {
+            InModuleScope -ScriptBlock {
+                $script:mockSqlRSSetupInstance.Test() | Should -BeFalse
+            }
+        }
+    }
+}
