05f460b57bcffcbb8297240232481e98f778d918
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-04-02 07:18:03 +0200

    Revert debug code

--- tests/Integration/Resources/DSC_SqlRSSetup.Integration.Tests.ps1
+++ tests/Integration/Resources/DSC_SqlRSSetup.Integration.Tests.ps1
@@ -178,25 +178,24 @@ Describe "$($script:dscResourceName)_Integration" -Tag @('Integration_SQL2017', 
 
             if (Test-ContinuousIntegrationTaskCategory -Category 'Integration_SQL2017')
             {
-                Write-Verbose -Message ($resourceCurrentState | Out-String) -Verbose
+                ## Uncomment this line to see the registry key values.
                 #Write-Verbose -Message ((reg query "HKLM\SOFTWARE\Microsoft\Microsoft SQL Server" /s) | Out-String) -Verbose
-                Write-Verbose -Message (Get-SqlDscRSSetupConfiguration -InstanceName ('{0}' -f $ConfigurationData.AllNodes.InstanceName) | Out-String) -Verbose
 
                 $resourceCurrentState.ProductVersion | Should -BeGreaterThan ([System.Version] '14.0.0.0')
             }
 
             if (Test-ContinuousIntegrationTaskCategory -Category 'Integration_SQL2019')
             {
+                ## Uncomment this line to see the registry key values.
                 #Write-Verbose -Message ((reg query "HKLM\SOFTWARE\Microsoft\Microsoft SQL Server" /s) | Out-String) -Verbose
-                Write-Verbose -Message (Get-SqlDscRSSetupConfiguration -InstanceName ('{0}' -f $ConfigurationData.AllNodes.InstanceName) | Out-String) -Verbose
 
                 $resourceCurrentState.ProductVersion | Should -BeGreaterThan ([System.Version] '15.0.0.0')
             }
 
             if (Test-ContinuousIntegrationTaskCategory -Category 'Integration_SQL2022')
             {
+                ## Uncomment this line to see the registry key values.
                 #Write-Verbose -Message ((reg query "HKLM\SOFTWARE\Microsoft\Microsoft SQL Server" /s) | Out-String) -Verbose
-                Write-Verbose -Message (Get-SqlDscRSSetupConfiguration -InstanceName ('{0}' -f $ConfigurationData.AllNodes.InstanceName) | Out-String) -Verbose
 
                 $resourceCurrentState.ProductVersion | Should -BeGreaterThan ([System.Version] '16.0.0.0')
             }
