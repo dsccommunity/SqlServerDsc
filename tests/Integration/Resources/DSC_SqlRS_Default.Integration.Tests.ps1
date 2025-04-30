e46f900696e74a1bcd3bd9401b6032c711c8a6d8
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-04-30 15:40:59 +0200

    Refactor tests to redirect output streams correctly

--- tests/Integration/Resources/DSC_SqlRS_Default.Integration.Tests.ps1
+++ tests/Integration/Resources/DSC_SqlRS_Default.Integration.Tests.ps1
@@ -10,7 +10,7 @@ BeforeDiscovery {
             if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
             {
                 # Redirect all streams to $null, except the error stream (stream 2)
-                & "$PSScriptRoot/../../../build.ps1" -Tasks 'noop' 2>&1 4>&1 5>&1 6>&1 > $null
+                & "$PSScriptRoot/../../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
             }
 
             # If the dependencies has not been resolved, this will throw an error.
