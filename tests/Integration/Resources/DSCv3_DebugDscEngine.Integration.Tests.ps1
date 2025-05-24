0fa131c813e787179d9bc047c29a7f7fa33c4ce6
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-05-24 10:31:35 +0200

    Enhance verbose logging to include detailed result output in DebugDscEngine integration tests

--- tests/Integration/Resources/DSCv3_DebugDscEngine.Integration.Tests.ps1
+++ tests/Integration/Resources/DSCv3_DebugDscEngine.Integration.Tests.ps1
@@ -53,7 +53,8 @@ Describe "$($script:dscResourceFriendlyName)_Integration" -Tag @('Integration_SQ
 
             $result = dsc --trace-level trace resource get --resource SqlServerDsc/DebugDscEngine --output-format json --input ($desiredParameters | ConvertTo-Json -Compress) | ConvertFrom-Json
 
-            Write-Verbose -Message "Result:`n$($result | ConvertTo-Json | Out-String)" -Verbose
+            Write-Verbose -Message "Result (all):`n$($result | ConvertTo-Json | Out-String)" -Verbose
+            Write-Verbose -Message "Result (actualState):`n$($result.actualState | ConvertTo-Json | Out-String)" -Verbose
 
             $dscExitCode = $LASTEXITCODE # cSpell: ignore LASTEXITCODE
 
