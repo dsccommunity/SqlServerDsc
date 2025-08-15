8f3d576331f4b041441df1ed80cefd4721d0d167
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-08-15 19:09:57 +0200

    Remove unnecessary suppression of PSUseOutputTypeCorrectly warning in Get-SqlDscLogin function

--- source/Public/Get-SqlDscLogin.ps1
+++ source/Public/Get-SqlDscLogin.ps1
@@ -29,7 +29,6 @@
 #>
 function Get-SqlDscLogin
 {
-    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseOutputTypeCorrectly', '', Justification = 'Because the rule does not understands that the command returns [System.String[]] when using , (comma) in the return statement')]
     [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('UseSyntacticallyCorrectExamples', '', Justification = 'Because the rule does not yet support parsing the code when a parameter type is not available. The ScriptAnalyzer rule UseSyntacticallyCorrectExamples will always error in the editor due to https://github.com/indented-automation/Indented.ScriptAnalyzerRules/issues/8.')]
     [OutputType([Microsoft.SqlServer.Management.Smo.Login[]])]
     [CmdletBinding()]
