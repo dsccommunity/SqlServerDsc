33dc0e21ce84cb89b2ceff6e20a02eaa3b78116f
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-08-16 10:53:58 +0200

    Fix comments for clarity and correct grammatical errors in Get-SqlDscLogin integration tests

--- tests/Unit/Public/Get-SqlDscLogin.Tests.ps1
+++ tests/Unit/Public/Get-SqlDscLogin.Tests.ps1
@@ -6,14 +6,14 @@ BeforeDiscovery {
     {
         if (-not (Get-Module -Name 'DscResource.Test'))
         {
-            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            # Assumes dependencies have been resolved, so if this module is not available, run 'noop' task.
             if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
             {
                 # Redirect all streams to $null, except the error stream (stream 2)
                 & "$PSScriptRoot/../../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
             }
 
-            # If the dependencies has not been resolved, this will throw an error.
+            # If the dependencies have not been resolved, this will throw an error.
             Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
         }
     }
@@ -75,7 +75,7 @@ Describe 'Get-SqlDscLogin' -Tag 'Public' {
         $result.ParameterListAsString | Should -Be $MockExpectedParameters
     }
 
-    Context 'When no login exist' {
+    Context 'When no login exists' {
         BeforeAll {
             $mockServerObject = New-Object -TypeName 'Microsoft.SqlServer.Management.Smo.Server' |
                 Add-Member -MemberType 'ScriptProperty' -Name 'Logins' -Value {
