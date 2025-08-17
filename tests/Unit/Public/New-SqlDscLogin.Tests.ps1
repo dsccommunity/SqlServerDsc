6343c437bbf3985b6bed2ee584fe27f295cb9d64
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-08-17 19:34:27 +0200

    Enhance New-SqlDscLogin tests by adding comprehensive parameter set validations and improving command metadata checks for better test coverage and reliability.

--- tests/Unit/Public/New-SqlDscLogin.Tests.ps1
+++ tests/Unit/Public/New-SqlDscLogin.Tests.ps1
@@ -1,4 +1,4 @@
-[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '', Justification = 'Pester discovery/context variables are declared before usage; this suppression aligns with repository test patterns.')]
 [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', '', Justification = 'Parameter is used in test mocks.')]
 param ()
 
@@ -51,6 +51,215 @@ AfterAll {
 }
 
 Describe 'New-SqlDscLogin' -Tag 'Public' {
+    It 'Should have the correct parameters in parameter set <MockParameterSetName>' -ForEach @(
+        @{
+            MockParameterSetName = 'SqlLogin'
+            MockExpectedParameters = '-ServerObject <Server> -Name <string> -SqlLogin -SecurePassword <securestring> [-DefaultDatabase <string>] [-DefaultLanguage <string>] [-PasswordExpirationEnabled] [-PasswordPolicyEnforced] [-MustChangePassword] [-IsHashed] [-Disabled] [-Force] [-PassThru] [-WhatIf] [-Confirm] [<CommonParameters>]'
+        }
+        @{
+            MockParameterSetName = 'WindowsUser'
+            MockExpectedParameters = '-ServerObject <Server> -Name <string> -WindowsUser [-DefaultDatabase <string>] [-DefaultLanguage <string>] [-Disabled] [-Force] [-PassThru] [-WhatIf] [-Confirm] [<CommonParameters>]'
+        }
+        @{
+            MockParameterSetName = 'WindowsGroup'
+            MockExpectedParameters = '-ServerObject <Server> -Name <string> -WindowsGroup [-DefaultDatabase <string>] [-DefaultLanguage <string>] [-Disabled] [-Force] [-PassThru] [-WhatIf] [-Confirm] [<CommonParameters>]'
+        }
+        @{
+            MockParameterSetName = 'Certificate'
+            MockExpectedParameters = '-ServerObject <Server> -Name <string> -Certificate -CertificateName <string> [-DefaultDatabase <string>] [-DefaultLanguage <string>] [-Disabled] [-Force] [-PassThru] [-WhatIf] [-Confirm] [<CommonParameters>]'
+        }
+        @{
+            MockParameterSetName = 'AsymmetricKey'
+            MockExpectedParameters = '-ServerObject <Server> -Name <string> -AsymmetricKey -AsymmetricKeyName <string> [-DefaultDatabase <string>] [-DefaultLanguage <string>] [-Disabled] [-Force] [-PassThru] [-WhatIf] [-Confirm] [<CommonParameters>]'
+        }
+    ) {
+        $result = (Get-Command -Name 'New-SqlDscLogin').ParameterSets |
+            Where-Object -FilterScript {
+                $_.Name -eq $mockParameterSetName
+            } |
+            Select-Object -Property @(
+                @{
+                    Name = 'ParameterSetName'
+                    Expression = { $_.Name }
+                },
+                @{
+                    Name = 'ParameterListAsString'
+                    Expression = { $_.ToString() }
+                }
+            )
+
+        $result.ParameterSetName | Should -Be $MockParameterSetName
+        $result.ParameterListAsString | Should -Be $MockExpectedParameters
+    }
+
+    It 'Should have the correct command metadata' {
+        $command = Get-Command -Name 'New-SqlDscLogin'
+
+        $cmdletBindingAttribute = $command.ScriptBlock.Attributes |
+            Where-Object -FilterScript { $_ -is [System.Management.Automation.CmdletBindingAttribute] }
+
+        $cmdletBindingAttribute.SupportsShouldProcess | Should -BeTrue
+        $cmdletBindingAttribute.ConfirmImpact | Should -Be 'Medium'
+        $cmdletBindingAttribute.DefaultParameterSetName | Should -Be 'WindowsUser'
+    }
+
+    It 'Should have correct parameter attributes for parameter set <ParameterSetName>' -ForEach @(
+        @{
+            ParameterSetName = 'SqlLogin'
+            ExpectedParameterTests = @(
+                @{ ParameterName = 'ServerObject'; IsMandatory = $true; ValueFromPipeline = $true; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Name'; IsMandatory = $true; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'SqlLogin'; IsMandatory = $true; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'SecurePassword'; IsMandatory = $true; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'DefaultDatabase'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'DefaultLanguage'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'PasswordExpirationEnabled'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'PasswordPolicyEnforced'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'MustChangePassword'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'IsHashed'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Disabled'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Force'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'PassThru'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'WindowsUser'; ShouldExist = $false }
+                @{ ParameterName = 'WindowsGroup'; ShouldExist = $false }
+                @{ ParameterName = 'Certificate'; ShouldExist = $false }
+                @{ ParameterName = 'AsymmetricKey'; ShouldExist = $false }
+                @{ ParameterName = 'CertificateName'; ShouldExist = $false }
+                @{ ParameterName = 'AsymmetricKeyName'; ShouldExist = $false }
+            )
+        }
+        @{
+            ParameterSetName = 'WindowsUser'
+            ExpectedParameterTests = @(
+                @{ ParameterName = 'ServerObject'; IsMandatory = $true; ValueFromPipeline = $true; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Name'; IsMandatory = $true; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'WindowsUser'; IsMandatory = $true; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'DefaultDatabase'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'DefaultLanguage'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Disabled'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Force'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'PassThru'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'SqlLogin'; ShouldExist = $false }
+                @{ ParameterName = 'WindowsGroup'; ShouldExist = $false }
+                @{ ParameterName = 'Certificate'; ShouldExist = $false }
+                @{ ParameterName = 'AsymmetricKey'; ShouldExist = $false }
+                @{ ParameterName = 'SecurePassword'; ShouldExist = $false }
+                @{ ParameterName = 'CertificateName'; ShouldExist = $false }
+                @{ ParameterName = 'AsymmetricKeyName'; ShouldExist = $false }
+                @{ ParameterName = 'PasswordExpirationEnabled'; ShouldExist = $false }
+                @{ ParameterName = 'PasswordPolicyEnforced'; ShouldExist = $false }
+                @{ ParameterName = 'MustChangePassword'; ShouldExist = $false }
+                @{ ParameterName = 'IsHashed'; ShouldExist = $false }
+            )
+        }
+        @{
+            ParameterSetName = 'WindowsGroup'
+            ExpectedParameterTests = @(
+                @{ ParameterName = 'ServerObject'; IsMandatory = $true; ValueFromPipeline = $true; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Name'; IsMandatory = $true; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'WindowsGroup'; IsMandatory = $true; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'DefaultDatabase'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'DefaultLanguage'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Disabled'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Force'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'PassThru'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'SqlLogin'; ShouldExist = $false }
+                @{ ParameterName = 'WindowsUser'; ShouldExist = $false }
+                @{ ParameterName = 'Certificate'; ShouldExist = $false }
+                @{ ParameterName = 'AsymmetricKey'; ShouldExist = $false }
+                @{ ParameterName = 'SecurePassword'; ShouldExist = $false }
+                @{ ParameterName = 'CertificateName'; ShouldExist = $false }
+                @{ ParameterName = 'AsymmetricKeyName'; ShouldExist = $false }
+                @{ ParameterName = 'PasswordExpirationEnabled'; ShouldExist = $false }
+                @{ ParameterName = 'PasswordPolicyEnforced'; ShouldExist = $false }
+                @{ ParameterName = 'MustChangePassword'; ShouldExist = $false }
+                @{ ParameterName = 'IsHashed'; ShouldExist = $false }
+            )
+        }
+        @{
+            ParameterSetName = 'Certificate'
+            ExpectedParameterTests = @(
+                @{ ParameterName = 'ServerObject'; IsMandatory = $true; ValueFromPipeline = $true; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Name'; IsMandatory = $true; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Certificate'; IsMandatory = $true; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'CertificateName'; IsMandatory = $true; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'DefaultDatabase'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'DefaultLanguage'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Disabled'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Force'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'PassThru'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'SqlLogin'; ShouldExist = $false }
+                @{ ParameterName = 'WindowsUser'; ShouldExist = $false }
+                @{ ParameterName = 'WindowsGroup'; ShouldExist = $false }
+                @{ ParameterName = 'AsymmetricKey'; ShouldExist = $false }
+                @{ ParameterName = 'SecurePassword'; ShouldExist = $false }
+                @{ ParameterName = 'AsymmetricKeyName'; ShouldExist = $false }
+                @{ ParameterName = 'PasswordExpirationEnabled'; ShouldExist = $false }
+                @{ ParameterName = 'PasswordPolicyEnforced'; ShouldExist = $false }
+                @{ ParameterName = 'MustChangePassword'; ShouldExist = $false }
+                @{ ParameterName = 'IsHashed'; ShouldExist = $false }
+            )
+        }
+        @{
+            ParameterSetName = 'AsymmetricKey'
+            ExpectedParameterTests = @(
+                @{ ParameterName = 'ServerObject'; IsMandatory = $true; ValueFromPipeline = $true; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Name'; IsMandatory = $true; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'AsymmetricKey'; IsMandatory = $true; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'AsymmetricKeyName'; IsMandatory = $true; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'DefaultDatabase'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'DefaultLanguage'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Disabled'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'Force'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'PassThru'; IsMandatory = $false; ValueFromPipeline = $false; ValueFromPipelineByPropertyName = $false }
+                @{ ParameterName = 'SqlLogin'; ShouldExist = $false }
+                @{ ParameterName = 'WindowsUser'; ShouldExist = $false }
+                @{ ParameterName = 'WindowsGroup'; ShouldExist = $false }
+                @{ ParameterName = 'Certificate'; ShouldExist = $false }
+                @{ ParameterName = 'SecurePassword'; ShouldExist = $false }
+                @{ ParameterName = 'CertificateName'; ShouldExist = $false }
+                @{ ParameterName = 'PasswordExpirationEnabled'; ShouldExist = $false }
+                @{ ParameterName = 'PasswordPolicyEnforced'; ShouldExist = $false }
+                @{ ParameterName = 'MustChangePassword'; ShouldExist = $false }
+                @{ ParameterName = 'IsHashed'; ShouldExist = $false }
+            )
+        }
+    ) {
+        $command = Get-Command -Name 'New-SqlDscLogin'
+
+        # Helper function to get parameter attribute for a specific parameter set
+        $getParameterAttribute = {
+            param($ParameterName, $ParameterSetName)
+
+            $parameter = $command.Parameters[$ParameterName]
+            if ($null -eq $parameter) {
+                return $null
+            }
+
+            $parameterAttribute = $parameter.Attributes |
+                Where-Object -FilterScript {
+                    $_ -is [System.Management.Automation.ParameterAttribute] -and
+                    ($_.ParameterSetName -eq $ParameterSetName -or $_.ParameterSetName -eq '__AllParameterSets')
+                } |
+                Select-Object -First 1
+
+            return $parameterAttribute
+        }
+
+        foreach ($parameterTest in $ExpectedParameterTests) {
+            $parameterAttribute = & $getParameterAttribute -ParameterName $parameterTest.ParameterName -ParameterSetName $ParameterSetName
+
+            if ($parameterTest.ContainsKey('ShouldExist') -and $parameterTest.ShouldExist -eq $false) {
+                $parameterAttribute | Should -BeNullOrEmpty -Because "Parameter '$($parameterTest.ParameterName)' should not exist in parameter set '$ParameterSetName'"
+            } else {
+                $parameterAttribute | Should -Not -BeNullOrEmpty -Because "Parameter '$($parameterTest.ParameterName)' should exist in parameter set '$ParameterSetName'"
+                $parameterAttribute.Mandatory | Should -Be $parameterTest.IsMandatory -Because "Parameter '$($parameterTest.ParameterName)' mandatory setting should be $($parameterTest.IsMandatory) in parameter set '$ParameterSetName'"
+                $parameterAttribute.ValueFromPipeline | Should -Be $parameterTest.ValueFromPipeline -Because "Parameter '$($parameterTest.ParameterName)' ValueFromPipeline setting should be $($parameterTest.ValueFromPipeline) in parameter set '$ParameterSetName'"
+                $parameterAttribute.ValueFromPipelineByPropertyName | Should -Be $parameterTest.ValueFromPipelineByPropertyName -Because "Parameter '$($parameterTest.ParameterName)' ValueFromPipelineByPropertyName setting should be $($parameterTest.ValueFromPipelineByPropertyName) in parameter set '$ParameterSetName'"
+            }
+        }
+    }
+
     Context 'When using parameter Confirm with value $false' {
         BeforeAll {
             Mock -CommandName Test-SqlDscIsLogin -MockWith {
