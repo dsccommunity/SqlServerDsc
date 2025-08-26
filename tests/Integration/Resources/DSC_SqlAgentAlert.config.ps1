207ce9c635855b82e25dc7ad34d77d2af960e08d
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-08-26 18:18:35 +0200

    Add support for MessageId in SqlAgentAlert configuration and tests

--- tests/Integration/Resources/DSC_SqlAgentAlert.config.ps1
+++ tests/Integration/Resources/DSC_SqlAgentAlert.config.ps1
@@ -24,8 +24,9 @@ else
                 ServerName      = $env:COMPUTERNAME
                 InstanceName    = 'DSCSQLTEST'
 
-                Name            = 'Sev17'
+                Name            = 'MockAlert'
                 Severity        = '17'
+                MessageId       = '50001'
 
                 CertificateFile = $env:DscPublicCertificatePath
             }
@@ -60,6 +61,57 @@ Configuration DSC_SqlAgentAlert_Add_Config
 
 <#
     .SYNOPSIS
+        Changes a SQL Agent alert to use MessageId instead of Severity.
+#>
+Configuration DSC_SqlAgentAlert_ChangeToMessageId_Config
+{
+    Import-DscResource -ModuleName 'SqlServerDsc'
+
+    node $AllNodes.NodeName
+    {
+        # First, create the custom system message
+        SqlScriptQuery 'CreateCustomMessage'
+        {
+            Id                   = 'CreateCustomMessage'
+            InstanceName         = $Node.InstanceName
+            ServerName           = $Node.ServerName
+            SetQuery             = "
+                IF NOT EXISTS (SELECT 1 FROM sys.messages WHERE message_id = $($Node.MessageId) AND language_id = 1033)
+                BEGIN
+                    EXEC sp_addmessage
+                        @msgnum = $($Node.MessageId),
+                        @severity = 16,
+                        @msgtext = N'Custom test message for SqlAgentAlert integration test',
+                        @lang = 'us_english'
+                END
+            "
+            TestQuery            = "SELECT 1 FROM sys.messages WHERE message_id = $($Node.MessageId) AND language_id = 1033"
+            GetQuery             = "SELECT message_id, text FROM sys.messages WHERE message_id = $($Node.MessageId) AND language_id = 1033"
+
+            PsDscRunAsCredential = New-Object `
+                -TypeName System.Management.Automation.PSCredential `
+                -ArgumentList @($Node.Username, (ConvertTo-SecureString -String $Node.Password -AsPlainText -Force))
+        }
+
+        SqlAgentAlert 'Integration_Test'
+        {
+            Ensure               = 'Present'
+            ServerName           = $Node.ServerName
+            InstanceName         = $Node.InstanceName
+            Name                 = $Node.Name
+            MessageId            = $Node.MessageId
+
+            PsDscRunAsCredential = New-Object `
+                -TypeName System.Management.Automation.PSCredential `
+                -ArgumentList @($Node.Username, (ConvertTo-SecureString -String $Node.Password -AsPlainText -Force))
+
+            DependsOn            = '[SqlScriptQuery]CreateCustomMessage'
+        }
+    }
+}
+
+<#
+    .SYNOPSIS
         Removes a SQL Agent alert.
 #>
 Configuration DSC_SqlAgentAlert_Remove_Config
@@ -74,11 +126,34 @@ Configuration DSC_SqlAgentAlert_Remove_Config
             ServerName           = $Node.ServerName
             InstanceName         = $Node.InstanceName
             Name                 = $Node.Name
-            Severity             = $Node.Severity
 
             PsDscRunAsCredential = New-Object `
                 -TypeName System.Management.Automation.PSCredential `
                 -ArgumentList @($Node.Username, (ConvertTo-SecureString -String $Node.Password -AsPlainText -Force))
+        }
+
+        # Clean up the custom system message
+        SqlScriptQuery 'RemoveCustomMessage'
+        {
+            Id                   = 'RemoveCustomMessage'
+            InstanceName         = $Node.InstanceName
+            ServerName           = $Node.ServerName
+            SetQuery             = "
+                IF EXISTS (SELECT 1 FROM sys.messages WHERE message_id = $($Node.MessageId) AND language_id = 1033)
+                BEGIN
+                    EXEC sp_dropmessage
+                        @msgnum = $($Node.MessageId),
+                        @lang = 'us_english'
+                END
+            "
+            TestQuery            = "SELECT CASE WHEN NOT EXISTS (SELECT 1 FROM sys.messages WHERE message_id = $($Node.MessageId) AND language_id = 1033) THEN 1 ELSE 0 END AS MessageRemoved"
+            GetQuery             = "SELECT COUNT(*) as MessageCount FROM sys.messages WHERE message_id = $($Node.MessageId) AND language_id = 1033"
+
+            PsDscRunAsCredential = New-Object `
+                -TypeName System.Management.Automation.PSCredential `
+                -ArgumentList @($Node.Username, (ConvertTo-SecureString -String $Node.Password -AsPlainText -Force))
+
+            DependsOn            = '[SqlAgentAlert]Integration_Test'
         }
     }
 }
