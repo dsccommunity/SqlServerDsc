bc3112fd85a440045f7fa25b8640533f4d521920
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-08-31 19:17:14 +0200

    Reorganize server permissions in SqlServerPermission enum for clarity and consistency

--- source/Enum/002.ServerPermission.ps1
+++ source/Enum/002.ServerPermission.ps1
@@ -1,41 +1,63 @@
 <#
     .SYNOPSIS
         The possible server permissions that can be granted, denied, or revoked.
+
+    .NOTES
+        The available permissions can be seen in the ServerPermission Class documentation:
+        https://learn.microsoft.com/en-us/dotnet/api/microsoft.sqlserver.management.smo.serverpermission
 #>
 enum SqlServerPermission
 {
+    # cSpell:ignore securables
     AdministerBulkOperations = 1
-    AlterAnyServerAudit
-    AlterAnyCredential
+    AlterAnyAvailabilityGroup
     AlterAnyConnection
+    AlterAnyCredential
     AlterAnyDatabase
-    AlterAnyEventNotification
     AlterAnyEndpoint
-    AlterAnyLogin
+    AlterAnyEventNotification
+    AlterAnyEventSession
+    AlterAnyEventSessionAddEvent
+    AlterAnyEventSessionAddTarget
+    AlterAnyEventSessionDisable
+    AlterAnyEventSessionDropEvent
+    AlterAnyEventSessionDropTarget
+    AlterAnyEventSessionEnable
+    AlterAnyEventSessionOption
     AlterAnyLinkedServer
+    AlterAnyLogin
+    AlterAnyServerAudit
+    AlterAnyServerRole
     AlterResources
     AlterServerState
     AlterSettings
     AlterTrace
     AuthenticateServer
-    ControlServer
+    ConnectAnyDatabase
     ConnectSql
+    ControlServer
     CreateAnyDatabase
+    CreateAnyEventSession
+    CreateAvailabilityGroup
     CreateDdlEventNotification
     CreateEndpoint
-    CreateTraceEventNotification
-    Shutdown
-    ViewAnyDefinition
-    ViewAnyDatabase
-    ViewServerState
-    ExternalAccessAssembly
-    UnsafeAssembly
-    AlterAnyServerRole
+    CreateLogin
     CreateServerRole
-    AlterAnyAvailabilityGroup
-    CreateAvailabilityGroup
-    AlterAnyEventSession
-    SelectAllUserSecurables
-    ConnectAnyDatabase
+    CreateTraceEventNotification
+    DropAnyEventSession
+    ExternalAccessAssembly
     ImpersonateAnyLogin
+    SelectAllUserSecurables
+    Shutdown
+    UnsafeAssembly
+    ViewAnyCryptographicallySecuredDefinition
+    ViewAnyDatabase
+    ViewAnyDefinition
+    ViewAnyErrorLog
+    ViewAnyPerformanceDefinition
+    ViewAnySecurityDefinition
+    ViewServerPerformanceState
+    ViewServerSecurityAudit
+    ViewServerSecurityState
+    ViewServerState
 }
