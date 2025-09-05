b56bca4b207d2a4f525d926aac609f3db7344bed
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-09-05 12:55:30 +0200

    Add ErrorAction parameter to Connect-SqlDscDatabaseEngine for improved error handling in Deny-SqlDscServerPermission tests

--- tests/Integration/Commands/Deny-SqlDscServerPermission.Integration.Tests.ps1
+++ tests/Integration/Commands/Deny-SqlDscServerPermission.Integration.Tests.ps1
@@ -41,7 +41,7 @@ Describe 'Deny-SqlDscServerPermission' -Tag @('Integration_SQL2017', 'Integratio
 
         $script:mockSqlAdminCredential = [System.Management.Automation.PSCredential]::new($mockSqlAdministratorUserName, $mockSqlAdministratorPassword)
 
-        $script:serverObject = Connect-SqlDscDatabaseEngine -InstanceName $script:mockInstanceName -Credential $script:mockSqlAdminCredential
+        $script:serverObject = Connect-SqlDscDatabaseEngine -InstanceName $script:mockInstanceName -Credential $script:mockSqlAdminCredential -ErrorAction 'Stop'
 
         # Use persistent test login and role created by earlier integration tests
         $script:testLoginName = 'IntegrationTestSqlLogin'
