a9117e5cdd237ebd95a18d5285435e1905a7cf9b
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-09-06 19:49:33 +0200

    Remove redundant OutputType attribute from Set-SqlDscDatabaseDefault function

--- source/Public/Set-SqlDscDatabaseDefault.ps1
+++ source/Public/Set-SqlDscDatabaseDefault.ps1
@@ -71,7 +71,6 @@
 function Set-SqlDscDatabaseDefault
 {
     [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('UseSyntacticallyCorrectExamples', '', Justification = 'Because the rule does not yet support parsing the code when a parameter type is not available. The ScriptAnalyzer rule UseSyntacticallyCorrectExamples will always error in the editor due to https://github.com/indented-automation/Indented.ScriptAnalyzerRules/issues/8.')]
-    [OutputType()]
     [OutputType([Microsoft.SqlServer.Management.Smo.Database])]
     [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
     param
