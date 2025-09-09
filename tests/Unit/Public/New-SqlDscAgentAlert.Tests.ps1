dad271f1585dce7cb15f06488e184233645fe968
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-09-09 14:02:41 +0200

    Add environment variable setup and cleanup in BeforeAll and AfterAll blocks

--- tests/Unit/Public/New-SqlDscAgentAlert.Tests.ps1
+++ tests/Unit/Public/New-SqlDscAgentAlert.Tests.ps1
@@ -26,6 +26,8 @@ BeforeDiscovery {
 BeforeAll {
     $script:dscModuleName = 'SqlServerDsc'
 
+    $env:SqlServerDscCI = $true
+
     Import-Module -Name $script:dscModuleName -Force -ErrorAction 'Stop'
 
     # Load SMO stub types
@@ -41,6 +43,8 @@ AfterAll {
 
     # Unload the module being tested so that it doesn't impact any other tests.
     Get-Module -Name $script:dscModuleName -All | Remove-Module -Force
+
+    Remove-Item -Path 'env:SqlServerDscCI' -ErrorAction 'SilentlyContinue'
 }
 
 Describe 'New-SqlDscAgentAlert' -Tag 'Public' {
