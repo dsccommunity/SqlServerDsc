81587d976a5d6b510ca27783faf80fa909c55a2d
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-10-27 18:34:21 +0100

    Add suppression message for ScriptAnalyzer rule in Get-SqlDscCompatibilityLevel function

--- source/Public/Get-SqlDscCompatibilityLevel.ps1
+++ source/Public/Get-SqlDscCompatibilityLevel.ps1
@@ -45,6 +45,7 @@
 #>
 function Get-SqlDscCompatibilityLevel
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('UseSyntacticallyCorrectExamples', '', Justification = 'Because the rule does not yet support parsing the code when a parameter type is not available. The ScriptAnalyzer rule UseSyntacticallyCorrectExamples will always error in the editor due to https://github.com/indented-automation/Indented.ScriptAnalyzerRules/issues/8.')]
     [CmdletBinding(DefaultParameterSetName = 'ServerObject')]
     [OutputType([System.String[]])]
     param
