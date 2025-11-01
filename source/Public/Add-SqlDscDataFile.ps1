0e6880899671071ef287b8a2a572501e54de7c2e
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-11-01 19:07:46 +0100

    Add suppression for ScriptAnalyzer warning in Add-SqlDscDataFile function

--- source/Public/Add-SqlDscDataFile.ps1
+++ source/Public/Add-SqlDscDataFile.ps1
@@ -38,6 +38,7 @@
 #>
 function Add-SqlDscDataFile
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('UseSyntacticallyCorrectExamples', '', Justification = 'Because the rule does not yet support parsing the code when a parameter type is not available. The ScriptAnalyzer rule UseSyntacticallyCorrectExamples will always error in the editor due to https://github.com/indented-automation/Indented.ScriptAnalyzerRules/issues/8.')]
     [CmdletBinding()]
     [OutputType([Microsoft.SqlServer.Management.Smo.DataFile])]
     param
