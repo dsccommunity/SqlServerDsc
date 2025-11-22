0fcf82b4e826d8e24af6b7221c07bdc40b2002d1
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-11-22 10:30:13 +0100

    Add suppression message for ScriptAnalyzer rule in New-SqlDscFileGroup function

--- source/Public/New-SqlDscFileGroup.ps1
+++ source/Public/New-SqlDscFileGroup.ps1
@@ -80,6 +80,7 @@
 #>
 function New-SqlDscFileGroup
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('UseSyntacticallyCorrectExamples', '', Justification = 'Because the rule does not yet support parsing the code when a parameter type is not available. The ScriptAnalyzer rule UseSyntacticallyCorrectExamples will always error in the editor due to https://github.com/indented-automation/Indented.ScriptAnalyzerRules/issues/8.')]
     [CmdletBinding(DefaultParameterSetName = 'AsSpec', SupportsShouldProcess = $true, ConfirmImpact = 'High')]
     [OutputType([Microsoft.SqlServer.Management.Smo.FileGroup])]
     [OutputType([DatabaseFileGroupSpec])]
