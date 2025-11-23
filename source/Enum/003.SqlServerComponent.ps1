99c0667f3e1b04f6d1700b953219a4c4b1aeb10c
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-11-23 16:46:39 +0100

    Add SqlServerComponent enumeration to define SQL Server components

---
+++ source/Enum/003.SqlServerComponent.ps1
@@ -0,0 +1,23 @@
+<#
+    .SYNOPSIS
+        The SQL Server component installed on the system.
+
+    .DESCRIPTION
+        The SQL Server component installed on the system.
+#>
+enum SqlServerComponent
+{
+    BackwardCompatibility
+    BooksOnline
+    Connectivity
+    DataQualityClient
+    DataQualityServer
+    IntegrationServices
+    ManagementStudio
+    ManagementStudioAdvanced
+    MasterDataServices
+    ROpenRPackages
+    RServices
+    Replication
+    SoftwareDevelopmentKit
+}
