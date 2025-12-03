331e6530350fe9c0b91989df107bbabde9699428
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-12-03 22:02:16 +0100

    Update output documentation for Uninstall-SqlDscBIReportServer function to clarify return values

--- source/Public/Uninstall-SqlDscBIReportServer.ps1
+++ source/Public/Uninstall-SqlDscBIReportServer.ps1
@@ -29,15 +29,18 @@
     .PARAMETER PassThru
         If specified the command will return the setup process exit code.
 
-
     .INPUTS
         None.
 
-.OUTPUTS
+    .OUTPUTS
         `System.Int32`
 
-        When PassThru is specified the function will return the setup process exit
-        code.
+        The setup process exit code when PassThru is specified.
+
+    .OUTPUTS
+        None.
+
+        No output when PassThru is not specified.
 
     .EXAMPLE
         Uninstall-SqlDscBIReportServer -MediaPath 'E:\PowerBIReportServer.exe'
