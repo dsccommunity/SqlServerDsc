1930433b308e64f4a64101c02efba04934e606be
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-12-07 20:39:26 +0100

    Backup-SqlDscDatabase.Tests: Remove redundant failure context and update success checks to validate results

--- tests/Unit/Public/Backup-SqlDscDatabase.Tests.ps1
+++ tests/Unit/Public/Backup-SqlDscDatabase.Tests.ps1
@@ -139,34 +139,6 @@ Describe 'Backup-SqlDscDatabase' -Tag 'Public' {
         }
     }
 
-    Context 'When backup operation fails' {
-        BeforeAll {
-            $mockDatabaseObject = New-Object -TypeName 'Microsoft.SqlServer.Management.Smo.Database'
-            $mockDatabaseObject | Add-Member -MemberType 'NoteProperty' -Name 'Name' -Value 'TestDatabase' -Force
-            $mockDatabaseObject | Add-Member -MemberType 'NoteProperty' -Name 'RecoveryModel' -Value ([Microsoft.SqlServer.Management.Smo.RecoveryModel]::Full) -Force
-            $mockDatabaseObject | Add-Member -MemberType 'NoteProperty' -Name 'Status' -Value ([Microsoft.SqlServer.Management.Smo.DatabaseStatus]::Normal) -Force
-
-            $mockServerObject = New-Object -TypeName 'Microsoft.SqlServer.Management.Smo.Server'
-            $mockServerObject | Add-Member -MemberType 'NoteProperty' -Name 'InstanceName' -Value 'TestInstance' -Force
-
-            $mockDatabaseObject | Add-Member -MemberType 'NoteProperty' -Name 'Parent' -Value $mockServerObject -Force
-        }
-
-        It 'Should throw error when SqlBackup fails' {
-            # The stub Backup class has MockShouldThrowOnSqlBackup property
-            # Since we can't mock New-Object easily in the module scope,
-            # we test that the command properly handles exceptions by
-            # verifying the error behavior is correct
-            $expectedMessage = InModuleScope -ScriptBlock {
-                $script:localizedData.Database_Backup_Failed -f 'full', 'TestDatabase', 'TestInstance'
-            }
-
-            # The stub's SqlBackup will run successfully unless MockShouldThrowOnSqlBackup is set
-            # For now, test that the command executes without error for a successful backup
-            { Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -Force } | Should -Not -Throw
-        }
-    }
-
     Context 'When performing successful backups' {
         BeforeAll {
             $mockDatabaseObject = New-Object -TypeName 'Microsoft.SqlServer.Management.Smo.Database'
@@ -181,39 +153,57 @@ Describe 'Backup-SqlDscDatabase' -Tag 'Public' {
         }
 
         It 'Should perform full backup successfully using database object' {
-            { Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -Force } | Should -Not -Throw
+            $result = Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -Force
+
+            $result | Should -BeNullOrEmpty
         }
 
         It 'Should perform differential backup successfully' {
-            { Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase_Diff.bak' -BackupType 'Differential' -Force } | Should -Not -Throw
+            $result = Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase_Diff.bak' -BackupType 'Differential' -Force
+
+            $result | Should -BeNullOrEmpty
         }
 
         It 'Should perform log backup successfully' {
-            { Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.trn' -BackupType 'Log' -Force } | Should -Not -Throw
+            $result = Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.trn' -BackupType 'Log' -Force
+
+            $result | Should -BeNullOrEmpty
         }
 
         It 'Should perform backup with CopyOnly option' {
-            { Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -CopyOnly -Force } | Should -Not -Throw
+            $result = Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -CopyOnly -Force
+
+            $result | Should -BeNullOrEmpty
         }
 
         It 'Should perform backup with Compress option' {
-            { Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -Compress -Force } | Should -Not -Throw
+            $result = Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -Compress -Force
+
+            $result | Should -BeNullOrEmpty
         }
 
         It 'Should perform backup with Checksum option' {
-            { Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -Checksum -Force } | Should -Not -Throw
+            $result = Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -Checksum -Force
+
+            $result | Should -BeNullOrEmpty
         }
 
         It 'Should perform backup with Description option' {
-            { Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -Description 'Test backup description' -Force } | Should -Not -Throw
+            $result = Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -Description 'Test backup description' -Force
+
+            $result | Should -BeNullOrEmpty
         }
 
         It 'Should perform backup with RetainDays option' {
-            { Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -RetainDays 30 -Force } | Should -Not -Throw
+            $result = Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -RetainDays 30 -Force
+
+            $result | Should -BeNullOrEmpty
         }
 
         It 'Should perform backup with Initialize option' {
-            { Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -Initialize -Force } | Should -Not -Throw
+            $result = Backup-SqlDscDatabase -DatabaseObject $mockDatabaseObject -BackupFile 'C:\Backups\TestDatabase.bak' -Initialize -Force
+
+            $result | Should -BeNullOrEmpty
         }
     }
 
@@ -245,8 +235,9 @@ Describe 'Backup-SqlDscDatabase' -Tag 'Public' {
         It 'Should call Refresh when -Refresh parameter is specified' {
             $script:refreshCalled = $false
 
-            { Backup-SqlDscDatabase -ServerObject $mockServerObject -Name 'TestDatabase' -BackupFile 'C:\Backups\TestDatabase.bak' -Refresh -Force } | Should -Not -Throw
+            $result = Backup-SqlDscDatabase -ServerObject $mockServerObject -Name 'TestDatabase' -BackupFile 'C:\Backups\TestDatabase.bak' -Refresh -Force
 
+            $result | Should -BeNullOrEmpty
             $script:refreshCalled | Should -BeTrue
         }
     }
