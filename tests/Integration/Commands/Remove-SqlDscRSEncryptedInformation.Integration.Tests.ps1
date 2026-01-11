bf45d011e21539d10da048057a0b23a1cbf7a9a7
Johan Ljunggren <johan.ljunggren@shamn.se>
2026-01-11 10:07:08 +0100

    Add sleep delay in BeforeAll for Remove-SqlDscRSEncryptedInformation tests

--- tests/Integration/Commands/Remove-SqlDscRSEncryptedInformation.Integration.Tests.ps1
+++ tests/Integration/Commands/Remove-SqlDscRSEncryptedInformation.Integration.Tests.ps1
@@ -39,6 +39,10 @@ BeforeAll {
 #>
 
 Describe 'Remove-SqlDscRSEncryptedInformation' {
+    BeforeAll {
+        Start-Sleep -Seconds 300
+    }
+
     Context 'When removing encrypted information for SQL Server 2017 Reporting Services' -Tag @('Integration_SQL2017_RS') -Skip:$true {
         BeforeAll {
             $script:configuration = Get-SqlDscRSConfiguration -InstanceName 'SSRS' -ErrorAction 'Stop'
