134aa84daa528605b276ab8a0a6b1488498406e5
Johan Ljunggren <johan.ljunggren@shamn.se>
2026-01-18 18:52:15 +0100

    Fix credential handling and update comments in integration tests for SqlDatabase and SqlRSSetup resources

--- tests/Integration/Resources/DSCv3_SqlRSSetup.Integration.Tests.ps1
+++ tests/Integration/Resources/DSCv3_SqlRSSetup.Integration.Tests.ps1
@@ -6,20 +6,20 @@ BeforeDiscovery {
     {
         if (-not (Get-Module -Name 'DscResource.Test'))
         {
-            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            # Assumes dependencies have been resolved, so if this module is not available, run 'noop' task.
             if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
             {
-                # Redirect all streams to $null, except the error stream (stream 3) to match other tests
+                # Redirect all streams to $null, except the error stream (stream 2)
                 & "$PSScriptRoot/../../../build.ps1" -Tasks 'noop' 3>&1 4>&1 5>&1 6>&1 > $null
             }
 
-            # If the dependencies has not been resolved, this will throw an error.
+            # If the dependencies have not been resolved, this will throw an error.
             Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
         }
     }
     catch [System.IO.FileNotFoundException]
     {
-        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks noop" first.'
     }
 
     <#
