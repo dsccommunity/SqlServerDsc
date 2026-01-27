951df130292ed0670dc0233cf840aeda17f647c5
Johan Ljunggren <johan.ljunggren@shamn.se>
2026-01-27 15:38:04 +0100

    Remove default encryption handling for SQL Server 2025 in Connect-Sql function

--- source/Modules/SqlServerDsc.Common/Public/Connect-Sql.ps1
+++ source/Modules/SqlServerDsc.Common/Public/Connect-Sql.ps1
@@ -138,26 +138,6 @@ function Connect-Sql
     {
         $sqlConnectionContext.EncryptConnection = $true
     }
-    else
-    {
-        # SQL Server 2025 defaults to having encryption enabled.
-        $sqlConnectionContext.EncryptConnection = $false
-
-        <#
-            SQL Server 2025 defaults to trust server certificate.
-
-            TODO: We need a new parameter to control this behavior. Integration tests
-                DSC_SqlSecureConnection_AddSecureConnection_Config fails unless this is set.
-        #>
-        if ($sqlConnectionContext.PSObject.Properties.Name -contains 'TrustServerCertificate')
-        {
-            Write-Verbose -Message (
-                $script:localizedData.IgnoreServerCertificateTrust
-            )
-
-            $sqlConnectionContext.TrustServerCertificate = $false
-        }
-    }
 
     if ($PSCmdlet.ParameterSetName -eq 'SqlServer')
     {
