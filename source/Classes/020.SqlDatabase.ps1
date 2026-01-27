aa49742c034026e7f1f8e3547caf5fbc3d3bb690
Johan Ljunggren <johan.ljunggren@shamn.se>
2026-01-27 21:12:43 +0100

    Enhance CompatibilityLevel handling for SQL Server 2025 by converting numeric values to 'Version' prefixed format

--- source/Classes/020.SqlDatabase.ps1
+++ source/Classes/020.SqlDatabase.ps1
@@ -745,7 +745,21 @@ class SqlDatabase : SqlResourceBase
             # Only set CompatibilityLevel if it's a valid non-zero value
             if ($databaseObject.CompatibilityLevel -gt 0)
             {
-                $currentState.CompatibilityLevel = $databaseObject.CompatibilityLevel.ToString()
+                $compatValue = $databaseObject.CompatibilityLevel.ToString()
+
+                <#
+                    SMO may return numeric values (e.g. 170 for SQL Server 2025).
+                    Convert purely-numeric values to the 'Version' prefixed form
+                    so they match the ValidateSet values (Version80..Version170).
+                #>
+                if ($compatValue -match '^[0-9]+$')
+                {
+                    $currentState.CompatibilityLevel = 'Version{0}' -f $compatValue
+                }
+                else
+                {
+                    $currentState.CompatibilityLevel = $compatValue
+                }
             }
 
             $currentState.RecoveryModel = $databaseObject.RecoveryModel.ToString()
