ebd48211bc9562017203d2346371dbd7a97d6fe7
Johan Ljunggren <johan.ljunggren@shamn.se>
2026-06-30 19:04:37 +0200

    Update assertion for Sid in Get-TargetResource test to match expected value format

--- tests/Unit/DSC_SqlLogin.Tests.ps1
+++ tests/Unit/DSC_SqlLogin.Tests.ps1
@@ -150,7 +150,7 @@ Describe 'SqlLogin\Get-TargetResource' -Tag 'Get' {
                         $result.Disabled | Should-BeTrue
                         $result.DefaultDatabase | Should-Be 'master'
                         $result.Language | Should-Be 'us_english'
-                        $result.Sid | Should-BeCollection ([byte[]] -split ('B76150A66B38F64FAE9470091789AA66' -replace '..', '0x$& '))
+                        $result.Sid | Should-Be 'B76150A66B38F64FAE9470091789AA66'
 
                         if ($MockLoginType -eq 'SqlLogin')
                         {
